ac79fef6491a76c6f2066c1a95039b5e5dc6aa9e
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2024-11-29 12:40:10 +0100

    chore(release): disto files updated to version 1.1.0

--- distro/tomcat/assembly/src/start-cibseven.bat
+++ distro/tomcat/assembly/src/start-cibseven.bat
@@ -2,7 +2,7 @@
 
 set "CATALINA_HOME=%CD%\server\apache-tomcat-${version.tomcat}"
 
-echo "starting CIB seven 1.0 on Apache Tomcat ${version.tomcat}"
+echo "starting CIB seven 1.1.0 on Apache Tomcat ${version.tomcat}"
 
 cd server\apache-tomcat-${version.tomcat}\bin\
 start startup.bat
