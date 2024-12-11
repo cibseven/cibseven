camunda/camunda-bpm-platform

feat(core): add update and delete comment Java/Rest APIs (#4565)
DELETE /task/{taskId}/comment/{commentId}
  - deletes a comment of a given taskId and commentId
DELETE /task/{taskId}/comment
  - deletes all comments of a given taskId
PUT /task/comment
  - updates a comment
DELETE /process-instance/{processInstanceId}/comment/{commentId}
 - deletes a comment of a given processInstanceId and commentId
DELETE /process-instance/{processInstanceId}/comment.
 - deletes all comments of a given processInstanceId
PUT /process-instance/comment
 - updates a comment

related to: https://github.com/camunda/camunda-bpm-platform/issues/2551


---------

Co-authored-by: Prajwol Bhandari <prajwol.bhandari@fmr.com>
Co-authored-by: <yanavasileva@users.noreply.github.com>

engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/sub/runtime/impl/ProcessInstanceCommentResourceImpl.java
@@ -19,11 +19,12 @@ package org.camunda.bpm.engine.rest.sub.runtime.impl;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import javax.ws.rs.core.Response.Status;
-
+import org.camunda.bpm.engine.AuthorizationException;
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.exception.NullValueException;
 import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.identity.Authentication;
@@ -42,6 +43,7 @@ public class ProcessInstanceCommentResourceImpl implements ProcessInstanceCommen
     this.processInstanceId = processInstanceId;
   }
 
+  @Override
   public List<CommentDto> getComments() {
     if (!isHistoryEnabled()) {
       return Collections.emptyList();
@@ -57,6 +59,66 @@ public class ProcessInstanceCommentResourceImpl implements ProcessInstanceCommen
     }
 
     return comments;
+  }
+
+  /**
+   * Deletes a comment by a given commentId
+   */
+  @Override
+  public void deleteComment(String commentId) {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+
+    TaskService taskService = engine.getTaskService();
+    try {
+      taskService.deleteProcessInstanceComment(processInstanceId, commentId);
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  /**
+   * Updates message for a given processInstanceId and commentId
+   */
+  @Override
+  public void updateComment(CommentDto comment) {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+
+    TaskService taskService = engine.getTaskService();
+    try {
+      taskService.updateProcessInstanceComment(processInstanceId, comment.getId(), comment.getMessage());
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  /**
+   * Deletes all comments by a given processInstanceId
+   */
+  @Override
+  public void deleteComments() {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+    TaskService taskService = engine.getTaskService();
+
+    try {
+      taskService.deleteProcessInstanceComments(processInstanceId);
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  private void ensureHistoryEnabled(Status status) {
+    if (!isHistoryEnabled()) {
+      throw new InvalidRequestException(status, "History is not enabled");
+    }
   }
 
   private boolean isHistoryEnabled() {

engine/src/main/java/org/camunda/bpm/engine/impl/TaskServiceImpl.java
@@ -40,7 +40,9 @@ import org.camunda.bpm.engine.impl.cmd.CreateTaskCmd;
 import org.camunda.bpm.engine.impl.cmd.DelegateTaskCmd;
 import org.camunda.bpm.engine.impl.cmd.DeleteAttachmentCmd;
 import org.camunda.bpm.engine.impl.cmd.DeleteGroupIdentityLinkCmd;
+import org.camunda.bpm.engine.impl.cmd.DeleteProcessInstanceCommentCmd;
 import org.camunda.bpm.engine.impl.cmd.DeleteTaskCmd;
+import org.camunda.bpm.engine.impl.cmd.DeleteTaskCommentCmd;
 import org.camunda.bpm.engine.impl.cmd.DeleteUserIdentityLinkCmd;
 import org.camunda.bpm.engine.impl.cmd.GetAttachmentCmd;
 import org.camunda.bpm.engine.impl.cmd.GetAttachmentContentCmd;
@@ -71,6 +73,7 @@ import org.camunda.bpm.engine.impl.cmd.SetTaskNameCmd;
 import org.camunda.bpm.engine.impl.cmd.SetTaskOwnerCmd;
 import org.camunda.bpm.engine.impl.cmd.SetTaskPriorityCmd;
 import org.camunda.bpm.engine.impl.cmd.SetTaskVariablesCmd;
+import org.camunda.bpm.engine.impl.cmd.UpdateCommentCmd;
 import org.camunda.bpm.engine.impl.util.ExceptionUtil;
 import org.camunda.bpm.engine.task.Attachment;
 import org.camunda.bpm.engine.task.Comment;
@@ -378,6 +381,30 @@ public class TaskServiceImpl extends ServiceImpl implements TaskService {
 
   public Comment createComment(String taskId, String processInstance, String message) {
     return commandExecutor.execute(new AddCommentCmd(taskId, processInstance, message));
+  }
+
+  public void deleteTaskComment(String taskId, String commentId) {
+    commandExecutor.execute(new DeleteTaskCommentCmd(taskId, commentId));
+  }
+
+  public void deleteProcessInstanceComment(String processInstanceId, String commentId) {
+    commandExecutor.execute(new DeleteProcessInstanceCommentCmd(processInstanceId, commentId));
+  }
+
+  public void deleteTaskComments(String taskId) {
+    commandExecutor.execute(new DeleteTaskCommentCmd(taskId));
+  }
+
+  public void deleteProcessInstanceComments(String processInstanceId) {
+    commandExecutor.execute(new DeleteProcessInstanceCommentCmd(processInstanceId));
+  }
+
+  public void updateTaskComment(String taskId, String commentId, String message) {
+    commandExecutor.execute(new UpdateCommentCmd(taskId, null, commentId, message));
+  }
+
+  public void updateProcessInstanceComment(String processInstanceId, String commentId, String message) {
+    commandExecutor.execute(new UpdateCommentCmd(null, processInstanceId, commentId, message));
   }
 
   public List<Comment> getTaskComments(String taskId) {

engine/src/main/java/org/camunda/bpm/engine/impl/cmd/DeleteProcessInstanceCommentCmd.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.impl.cmd;
+
+import static org.camunda.bpm.engine.impl.util.EnsureUtil.ensureNotNull;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+import org.camunda.bpm.engine.BadUserRequestException;
+import org.camunda.bpm.engine.history.UserOperationLogEntry;
+import org.camunda.bpm.engine.impl.cfg.CommandChecker;
+import org.camunda.bpm.engine.impl.interceptor.Command;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
+import org.camunda.bpm.engine.impl.persistence.entity.CommentEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.PropertyChange;
+import org.camunda.bpm.engine.task.Comment;
+
+/**
+ * Command to delete a comment by a given commentId and processInstanceId or to delete all comments
+ * of a given processInstanceId
+ */
+
+public class DeleteProcessInstanceCommentCmd implements Command<Object>, Serializable {
+
+  private static final long serialVersionUID = 1L;
+  protected String commentId;
+  protected String processInstanceId;
+
+  public DeleteProcessInstanceCommentCmd(String processInstanceId, String commentId) {
+    this.processInstanceId = processInstanceId;
+    this.commentId = commentId;
+  }
+
+  public DeleteProcessInstanceCommentCmd(String processInstanceId) {
+    this.processInstanceId = processInstanceId;
+  }
+
+  @Override
+  public Object execute(CommandContext commandContext) {
+    ensureNotNull(BadUserRequestException.class, "processInstanceId", processInstanceId);
+
+    ExecutionEntity processInstance = commandContext.getExecutionManager().findExecutionById(processInstanceId);
+    ensureNotNull("No processInstance exists with processInstanceId: " + processInstanceId, "processInstance",
+        processInstance);
+
+    checkUpdateProcessInstanceById(processInstanceId, commandContext);
+    if (commentId != null ) {
+      CommentEntity comment = commandContext.getCommentManager()
+          .findCommentByProcessInstanceIdAndCommentId(processInstanceId, commentId);
+      if (comment != null) {
+        commandContext.getDbEntityManager().delete(comment);
+      }
+    } else { // delete all comments
+      List<Comment> comments = commandContext.getCommentManager().findCommentsByProcessInstanceId(processInstanceId);
+      if (!comments.isEmpty()) {
+        commandContext.getCommentManager()
+            .deleteCommentsByProcessInstanceIds(Collections.singletonList(processInstanceId));
+      }
+    }
+
+    logOperation(processInstance, commandContext);
+
+    return null;
+  }
+
+  protected void logOperation(ExecutionEntity processInstance, CommandContext commandContext) {
+    PropertyChange propertyChange = new PropertyChange("comment", null, null);
+    commandContext.getOperationLogManager()
+        .logCommentOperation(UserOperationLogEntry.OPERATION_TYPE_DELETE_COMMENT, processInstance, propertyChange);
+  }
+
+  protected void checkUpdateProcessInstanceById(String processInstanceId, CommandContext commandContext) {
+    for (CommandChecker checker : commandContext.getProcessEngineConfiguration().getCommandCheckers()) {
+      checker.checkUpdateProcessInstanceById(processInstanceId);
+    }
+  }
+
+}

engine/src/test/java/org/camunda/bpm/engine/test/api/authorization/AuthorizationTest.java
@@ -50,6 +50,7 @@ import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.runtime.CaseInstance;
 import org.camunda.bpm.engine.runtime.Job;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Comment;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.util.PluggableProcessEngineTest;
 import org.camunda.bpm.engine.variable.VariableMap;
@@ -69,6 +70,8 @@ public abstract class AuthorizationTest extends PluggableProcessEngineTest {
 
   protected static final String VARIABLE_NAME = "aVariableName";
   protected static final String VARIABLE_VALUE = "aVariableValue";
+  protected static final String TASK_ID = "myTask";
+
   protected List<String> deploymentIds = new ArrayList<>();
 
   @Before
@@ -285,6 +288,10 @@ public abstract class AuthorizationTest extends PluggableProcessEngineTest {
       taskService.deleteTask(taskId, cascade);
       return null;
     });
+  }
+
+  protected Comment createComment(String taskId, String processInstanceId, String message) {
+    return runWithoutAuthorization(() -> taskService.createComment(taskId, processInstanceId, message));
   }
 
   protected void addCandidateUser(final String taskId, final String user) {

cibseven/cibseven

Signed-off-by: Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>

engine-rest/engine-rest/src/main/java/org/cibseven/bpm/engine/rest/sub/runtime/impl/ProcessInstanceCommentResourceImpl.java
@@ -19,11 +19,12 @@ package org.cibseven.bpm.engine.rest.sub.runtime.impl;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import javax.ws.rs.core.Response.Status;
-
+import org.cibseven.bpm.engine.AuthorizationException;
 import org.cibseven.bpm.engine.IdentityService;
 import org.cibseven.bpm.engine.ProcessEngine;
+import org.cibseven.bpm.engine.TaskService;
+import org.cibseven.bpm.engine.exception.NullValueException;
 import org.cibseven.bpm.engine.history.HistoricProcessInstance;
 import org.cibseven.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.cibseven.bpm.engine.impl.identity.Authentication;
@@ -42,6 +43,7 @@ public class ProcessInstanceCommentResourceImpl implements ProcessInstanceCommen
     this.processInstanceId = processInstanceId;
   }
 
+  @Override
   public List<CommentDto> getComments() {
     if (!isHistoryEnabled()) {
       return Collections.emptyList();
@@ -57,6 +59,66 @@ public class ProcessInstanceCommentResourceImpl implements ProcessInstanceCommen
     }
 
     return comments;
+  }
+
+  /**
+   * Deletes a comment by a given commentId
+   */
+  @Override
+  public void deleteComment(String commentId) {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+
+    TaskService taskService = engine.getTaskService();
+    try {
+      taskService.deleteProcessInstanceComment(processInstanceId, commentId);
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  /**
+   * Updates message for a given processInstanceId and commentId
+   */
+  @Override
+  public void updateComment(CommentDto comment) {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+
+    TaskService taskService = engine.getTaskService();
+    try {
+      taskService.updateProcessInstanceComment(processInstanceId, comment.getId(), comment.getMessage());
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  /**
+   * Deletes all comments by a given processInstanceId
+   */
+  @Override
+  public void deleteComments() {
+    ensureHistoryEnabled(Status.FORBIDDEN);
+    ensureProcessInstanceExists(Status.NOT_FOUND);
+    TaskService taskService = engine.getTaskService();
+
+    try {
+      taskService.deleteProcessInstanceComments(processInstanceId);
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (NullValueException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e.getMessage());
+    }
+  }
+
+  private void ensureHistoryEnabled(Status status) {
+    if (!isHistoryEnabled()) {
+      throw new InvalidRequestException(status, "History is not enabled");
+    }
   }
 
   private boolean isHistoryEnabled() {

engine/src/main/java/org/cibseven/bpm/engine/impl/TaskServiceImpl.java
@@ -40,7 +40,9 @@ import org.cibseven.bpm.engine.impl.cmd.CreateTaskCmd;
 import org.cibseven.bpm.engine.impl.cmd.DelegateTaskCmd;
 import org.cibseven.bpm.engine.impl.cmd.DeleteAttachmentCmd;
 import org.cibseven.bpm.engine.impl.cmd.DeleteGroupIdentityLinkCmd;
+import org.cibseven.bpm.engine.impl.cmd.DeleteProcessInstanceCommentCmd;
 import org.cibseven.bpm.engine.impl.cmd.DeleteTaskCmd;
+import org.cibseven.bpm.engine.impl.cmd.DeleteTaskCommentCmd;
 import org.cibseven.bpm.engine.impl.cmd.DeleteUserIdentityLinkCmd;
 import org.cibseven.bpm.engine.impl.cmd.GetAttachmentCmd;
 import org.cibseven.bpm.engine.impl.cmd.GetAttachmentContentCmd;
@@ -71,6 +73,7 @@ import org.cibseven.bpm.engine.impl.cmd.SetTaskNameCmd;
 import org.cibseven.bpm.engine.impl.cmd.SetTaskOwnerCmd;
 import org.cibseven.bpm.engine.impl.cmd.SetTaskPriorityCmd;
 import org.cibseven.bpm.engine.impl.cmd.SetTaskVariablesCmd;
+import org.cibseven.bpm.engine.impl.cmd.UpdateCommentCmd;
 import org.cibseven.bpm.engine.impl.util.ExceptionUtil;
 import org.cibseven.bpm.engine.task.Attachment;
 import org.cibseven.bpm.engine.task.Comment;
@@ -378,6 +381,30 @@ public class TaskServiceImpl extends ServiceImpl implements TaskService {
 
   public Comment createComment(String taskId, String processInstance, String message) {
     return commandExecutor.execute(new AddCommentCmd(taskId, processInstance, message));
+  }
+
+  public void deleteTaskComment(String taskId, String commentId) {
+    commandExecutor.execute(new DeleteTaskCommentCmd(taskId, commentId));
+  }
+
+  public void deleteProcessInstanceComment(String processInstanceId, String commentId) {
+    commandExecutor.execute(new DeleteProcessInstanceCommentCmd(processInstanceId, commentId));
+  }
+
+  public void deleteTaskComments(String taskId) {
+    commandExecutor.execute(new DeleteTaskCommentCmd(taskId));
+  }
+
+  public void deleteProcessInstanceComments(String processInstanceId) {
+    commandExecutor.execute(new DeleteProcessInstanceCommentCmd(processInstanceId));
+  }
+
+  public void updateTaskComment(String taskId, String commentId, String message) {
+    commandExecutor.execute(new UpdateCommentCmd(taskId, null, commentId, message));
+  }
+
+  public void updateProcessInstanceComment(String processInstanceId, String commentId, String message) {
+    commandExecutor.execute(new UpdateCommentCmd(null, processInstanceId, commentId, message));
   }
 
   public List<Comment> getTaskComments(String taskId) {

engine/src/main/java/org/cibseven/bpm/engine/impl/cmd/DeleteProcessInstanceCommentCmd.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.engine.impl.cmd;
+
+import static org.cibseven.bpm.engine.impl.util.EnsureUtil.ensureNotNull;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+import org.cibseven.bpm.engine.BadUserRequestException;
+import org.cibseven.bpm.engine.history.UserOperationLogEntry;
+import org.cibseven.bpm.engine.impl.cfg.CommandChecker;
+import org.cibseven.bpm.engine.impl.interceptor.Command;
+import org.cibseven.bpm.engine.impl.interceptor.CommandContext;
+import org.cibseven.bpm.engine.impl.persistence.entity.CommentEntity;
+import org.cibseven.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.cibseven.bpm.engine.impl.persistence.entity.PropertyChange;
+import org.cibseven.bpm.engine.task.Comment;
+
+/**
+ * Command to delete a comment by a given commentId and processInstanceId or to delete all comments
+ * of a given processInstanceId
+ */
+
+public class DeleteProcessInstanceCommentCmd implements Command<Object>, Serializable {
+
+  private static final long serialVersionUID = 1L;
+  protected String commentId;
+  protected String processInstanceId;
+
+  public DeleteProcessInstanceCommentCmd(String processInstanceId, String commentId) {
+    this.processInstanceId = processInstanceId;
+    this.commentId = commentId;
+  }
+
+  public DeleteProcessInstanceCommentCmd(String processInstanceId) {
+    this.processInstanceId = processInstanceId;
+  }
+
+  @Override
+  public Object execute(CommandContext commandContext) {
+    ensureNotNull(BadUserRequestException.class, "processInstanceId", processInstanceId);
+
+    ExecutionEntity processInstance = commandContext.getExecutionManager().findExecutionById(processInstanceId);
+    ensureNotNull("No processInstance exists with processInstanceId: " + processInstanceId, "processInstance",
+        processInstance);
+
+    checkUpdateProcessInstanceById(processInstanceId, commandContext);
+    if (commentId != null ) {
+      CommentEntity comment = commandContext.getCommentManager()
+          .findCommentByProcessInstanceIdAndCommentId(processInstanceId, commentId);
+      if (comment != null) {
+        commandContext.getDbEntityManager().delete(comment);
+      }
+    } else { // delete all comments
+      List<Comment> comments = commandContext.getCommentManager().findCommentsByProcessInstanceId(processInstanceId);
+      if (!comments.isEmpty()) {
+        commandContext.getCommentManager()
+            .deleteCommentsByProcessInstanceIds(Collections.singletonList(processInstanceId));
+      }
+    }
+
+    logOperation(processInstance, commandContext);
+
+    return null;
+  }
+
+  protected void logOperation(ExecutionEntity processInstance, CommandContext commandContext) {
+    PropertyChange propertyChange = new PropertyChange("comment", null, null);
+    commandContext.getOperationLogManager()
+        .logCommentOperation(UserOperationLogEntry.OPERATION_TYPE_DELETE_COMMENT, processInstance, propertyChange);
+  }
+
+  protected void checkUpdateProcessInstanceById(String processInstanceId, CommandContext commandContext) {
+    for (CommandChecker checker : commandContext.getProcessEngineConfiguration().getCommandCheckers()) {
+      checker.checkUpdateProcessInstanceById(processInstanceId);
+    }
+  }
+
+}

engine/src/test/java/org/cibseven/bpm/engine/test/api/authorization/AuthorizationTest.java
@@ -50,6 +50,7 @@ import org.cibseven.bpm.engine.repository.ProcessDefinition;
 import org.cibseven.bpm.engine.runtime.CaseInstance;
 import org.cibseven.bpm.engine.runtime.Job;
 import org.cibseven.bpm.engine.runtime.ProcessInstance;
+import org.cibseven.bpm.engine.task.Comment;
 import org.cibseven.bpm.engine.task.Task;
 import org.cibseven.bpm.engine.test.util.PluggableProcessEngineTest;
 import org.cibseven.bpm.engine.variable.VariableMap;
@@ -69,6 +70,8 @@ public abstract class AuthorizationTest extends PluggableProcessEngineTest {
 
   protected static final String VARIABLE_NAME = "aVariableName";
   protected static final String VARIABLE_VALUE = "aVariableValue";
+  protected static final String TASK_ID = "myTask";
+
   protected List<String> deploymentIds = new ArrayList<>();
 
   @Before
@@ -285,6 +288,10 @@ public abstract class AuthorizationTest extends PluggableProcessEngineTest {
       taskService.deleteTask(taskId, cascade);
       return null;
     });
+  }
+
+  protected Comment createComment(String taskId, String processInstanceId, String message) {
+    return runWithoutAuthorization(() -> taskService.createComment(taskId, processInstanceId, message));
   }
 
   protected void addCandidateUser(final String taskId, final String user) {