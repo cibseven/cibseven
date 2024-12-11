camunda/camunda-bpm-platform

fix(openApi): add missing schema declarations
related to #3298

engine-rest/engine-rest-openapi/src/main/templates/paths/external-task/{id}/errorDetails/get.ftl
@@ -21,27 +21,21 @@
 
   "responses" : {
 
-    "200": {
-      "description": "Request successful.",
-      "content": {
-        "text/plain": {
-          "schema": {
-            "type": "string",
-            "description": "The error details for the external task."
-          },
-          "examples": {
-            "example-1": {
-              "value": "org.apache.ibatis.jdbc.RuntimeSqlException: org.apache.ibatis.jdbc.RuntimeSqlException: test cause
-                  at org.camunda.bpm.engine.test.api.externaltask.ExternalTaskServiceTest.testHandleFailureWithErrorDetails(ExternalTaskServiceTest.java:1424)
-                  at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-                  at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
-                  at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-                "
-            }
-          }
-        }
-      }
-    },
+    <@lib.response
+    code = "200"
+    mediaType= "text/plain"
+    desc = "Request successful."
+    contentDesc = "The error details for the external task."
+    examples = ['"example-1": {
+                       "summary": "GET `external-task/someId/errorDetails`",
+                       "description": "GET `external-task/someId/errorDetails`",
+                       "value": "org.apache.ibatis.jdbc.RuntimeSqlException: org.apache.ibatis.jdbc.RuntimeSqlException: test cause
+                                      at org.camunda.bpm.engine.test.api.externaltask.ExternalTaskServiceTest.testHandleFailureWithErrorDetails(ExternalTaskServiceTest.java:1424)
+                                      at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+                                      at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+                                      at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+                                      ..."
+                     }']/>
 
 
   <@lib.response

cibseven/cibseven

Signed-off-by: Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>

engine-rest/engine-rest-openapi/src/main/templates/paths/external-task/{id}/errorDetails/get.ftl
@@ -21,27 +21,21 @@
 
   "responses" : {
 
-    "200": {
-      "description": "Request successful.",
-      "content": {
-        "text/plain": {
-          "schema": {
-            "type": "string",
-            "description": "The error details for the external task."
-          },
-          "examples": {
-            "example-1": {
-              "value": "org.apache.ibatis.jdbc.RuntimeSqlException: org.apache.ibatis.jdbc.RuntimeSqlException: test cause
-                  at org.cibseven.bpm.engine.test.api.externaltask.ExternalTaskServiceTest.testHandleFailureWithErrorDetails(ExternalTaskServiceTest.java:1424)
-                  at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-                  at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
-                  at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-                "
-            }
-          }
-        }
-      }
-    },
+    <@lib.response
+    code = "200"
+    mediaType= "text/plain"
+    desc = "Request successful."
+    contentDesc = "The error details for the external task."
+    examples = ['"example-1": {
+                       "summary": "GET `external-task/someId/errorDetails`",
+                       "description": "GET `external-task/someId/errorDetails`",
+                       "value": "org.apache.ibatis.jdbc.RuntimeSqlException: org.apache.ibatis.jdbc.RuntimeSqlException: test cause
+                                      at org.cibseven.bpm.engine.test.api.externaltask.ExternalTaskServiceTest.testHandleFailureWithErrorDetails(ExternalTaskServiceTest.java:1424)
+                                      at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+                                      at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+                                      at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+                                      ..."
+                     }']/>
 
 
   <@lib.response