camunda/camunda-bpm-platform

fix(mssql): replace or with union in external task to improve performance

engine/src/test/java/org/camunda/bpm/engine/test/api/authorization/externaltask/ExternalTaskQueryAuthorizationTest.java
@@ -16,16 +16,22 @@
  */
 package org.camunda.bpm.engine.test.api.authorization.externaltask;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.camunda.bpm.engine.authorization.Authorization.ANY;
+import static org.camunda.bpm.engine.authorization.Permissions.ALL;
 import static org.camunda.bpm.engine.authorization.Permissions.READ;
 import static org.camunda.bpm.engine.authorization.Permissions.READ_INSTANCE;
 import static org.camunda.bpm.engine.authorization.Resources.PROCESS_DEFINITION;
 import static org.camunda.bpm.engine.authorization.Resources.PROCESS_INSTANCE;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import org.camunda.bpm.engine.externaltask.ExternalTaskQuery;
+import org.camunda.bpm.engine.externaltask.LockedExternalTask;
 import org.camunda.bpm.engine.test.api.authorization.AuthorizationTest;
+import org.camunda.commons.testing.ProcessEngineLoggingRule;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -34,9 +40,14 @@ import org.junit.Test;
  */
 public class ExternalTaskQueryAuthorizationTest extends AuthorizationTest {
 
+  protected static final String WORKER_ID = "aWorkerId";
+  protected static final String EXTERNAL_TASK_TOPIC = "externalTaskTopic";
   protected String deploymentId;
   protected String instance1Id;
   protected String instance2Id;
+
+  @Rule
+  public ProcessEngineLoggingRule loggingRule = new ProcessEngineLoggingRule();
 
   @Override
   @Before
@@ -134,5 +145,53 @@ public class ExternalTaskQueryAuthorizationTest extends AuthorizationTest {
 
     // then
     verifyQueryResults(query, 1);
+  }
+
+  @Test
+  public void shouldFetchAndLockWhenUserAuthorized() {
+    // given
+    createGrantAuthorization(PROCESS_DEFINITION, ANY, userId, ALL);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).hasSize(1);
+    assertThat(externalTasks).extracting(LockedExternalTask::getTopicName).first().isEqualTo(EXTERNAL_TASK_TOPIC);
+  }
+
+  @Test
+  public void shouldFetchAndLockWhenGroupAuthorized() {
+    // given
+    createGrantAuthorizationGroup(PROCESS_DEFINITION, ANY, groupId, ALL);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).hasSize(1);
+    assertThat(externalTasks).extracting(LockedExternalTask::getTopicName).first().isEqualTo(EXTERNAL_TASK_TOPIC);
+  }
+
+  @Test
+  public void shouldNotFetchAndLockWhenUnauthorized() {
+    // given
+    createGrantAuthorization(PROCESS_DEFINITION, ANY, userId, READ_INSTANCE);
+    createGrantAuthorizationGroup(PROCESS_DEFINITION, ANY, groupId, READ_INSTANCE);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).isEmpty();
   }
 }

cibseven/cibseven

Signed-off-by: Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>

engine/src/test/java/org/cibseven/bpm/engine/test/api/authorization/externaltask/ExternalTaskQueryAuthorizationTest.java
@@ -16,16 +16,22 @@
  */
 package org.cibseven.bpm.engine.test.api.authorization.externaltask;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.cibseven.bpm.engine.authorization.Authorization.ANY;
+import static org.cibseven.bpm.engine.authorization.Permissions.ALL;
 import static org.cibseven.bpm.engine.authorization.Permissions.READ;
 import static org.cibseven.bpm.engine.authorization.Permissions.READ_INSTANCE;
 import static org.cibseven.bpm.engine.authorization.Resources.PROCESS_DEFINITION;
 import static org.cibseven.bpm.engine.authorization.Resources.PROCESS_INSTANCE;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import org.cibseven.bpm.engine.externaltask.ExternalTaskQuery;
+import org.cibseven.bpm.engine.externaltask.LockedExternalTask;
 import org.cibseven.bpm.engine.test.api.authorization.AuthorizationTest;
+import org.cibseven.commons.testing.ProcessEngineLoggingRule;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -34,9 +40,14 @@ import org.junit.Test;
  */
 public class ExternalTaskQueryAuthorizationTest extends AuthorizationTest {
 
+  protected static final String WORKER_ID = "aWorkerId";
+  protected static final String EXTERNAL_TASK_TOPIC = "externalTaskTopic";
   protected String deploymentId;
   protected String instance1Id;
   protected String instance2Id;
+
+  @Rule
+  public ProcessEngineLoggingRule loggingRule = new ProcessEngineLoggingRule();
 
   @Override
   @Before
@@ -134,5 +145,53 @@ public class ExternalTaskQueryAuthorizationTest extends AuthorizationTest {
 
     // then
     verifyQueryResults(query, 1);
+  }
+
+  @Test
+  public void shouldFetchAndLockWhenUserAuthorized() {
+    // given
+    createGrantAuthorization(PROCESS_DEFINITION, ANY, userId, ALL);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).hasSize(1);
+    assertThat(externalTasks).extracting(LockedExternalTask::getTopicName).first().isEqualTo(EXTERNAL_TASK_TOPIC);
+  }
+
+  @Test
+  public void shouldFetchAndLockWhenGroupAuthorized() {
+    // given
+    createGrantAuthorizationGroup(PROCESS_DEFINITION, ANY, groupId, ALL);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).hasSize(1);
+    assertThat(externalTasks).extracting(LockedExternalTask::getTopicName).first().isEqualTo(EXTERNAL_TASK_TOPIC);
+  }
+
+  @Test
+  public void shouldNotFetchAndLockWhenUnauthorized() {
+    // given
+    createGrantAuthorization(PROCESS_DEFINITION, ANY, userId, READ_INSTANCE);
+    createGrantAuthorizationGroup(PROCESS_DEFINITION, ANY, groupId, READ_INSTANCE);
+
+    // when
+    List<LockedExternalTask> externalTasks = externalTaskService
+        .fetchAndLock(1, WORKER_ID)
+        .topic(EXTERNAL_TASK_TOPIC, 10)
+        .execute();
+
+    // then
+    assertThat(externalTasks).isEmpty();
   }
 }