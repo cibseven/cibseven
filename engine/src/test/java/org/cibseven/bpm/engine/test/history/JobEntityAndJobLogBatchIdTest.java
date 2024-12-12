camunda/camunda-bpm-platform

feat(engine): don't fail on change variables async when process instance does not exist
Related to https://github.com/camunda/camunda-bpm-platform/issues/4795

engine/src/test/java/org/camunda/bpm/engine/test/history/JobEntityAndJobLogBatchIdTest.java
@@ -17,6 +17,7 @@
 package org.camunda.bpm.engine.test.history;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.test.util.ExecutableProcessUtil.USER_TASK_PROCESS;
 
 import java.util.List;
 import java.util.Map;
@@ -83,14 +84,6 @@ public class JobEntityAndJobLogBatchIdTest {
     externalTaskService = engineRule.getExternalTaskService();
   }
 
-  private BpmnModelInstance getUserTaskProcess() {
-    return Bpmn.createExecutableProcess("process")
-        .startEvent()
-        .userTask("task1")
-        .endEvent()
-        .done();
-  }
-
   private BpmnModelInstance getTwoUserTasksProcess() {
     return Bpmn.createExecutableProcess("process")
         .startEvent()
@@ -111,7 +104,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_SetHistoricBatchRemovalTime() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
 
     // create historic Batch
@@ -159,7 +152,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_SetVariables() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.setVariablesAsync(List.of(process.getId()), Variables.createVariables().putValue("foo", "bar"));
@@ -250,7 +243,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_DeleteProcessInstances() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.deleteProcessInstancesAsync(List.of(process.getId()), null);
@@ -289,7 +282,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_Migration() {
     // given
-    ProcessDefinition sourceProcessDefinition = testRule.deployAndGetDefinition(getUserTaskProcess());
+    ProcessDefinition sourceProcessDefinition = testRule.deployAndGetDefinition(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
     ProcessDefinition targetProcessDefinition = testRule.deployAndGetDefinition(getTwoUserTasksProcess());
 
@@ -351,7 +344,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_RestartProcessInstance() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
     runtimeService.deleteProcessInstance(processInstance.getId(), null);
 
@@ -411,7 +404,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_UpdateProcessInstancesSuspendState() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.updateProcessInstanceSuspensionState()

engine/src/test/java/org/camunda/bpm/engine/test/util/ExecutableProcessUtil.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.util;
+
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+
+public class ExecutableProcessUtil {
+
+  public static final BpmnModelInstance USER_TASK_PROCESS = Bpmn.createExecutableProcess("process")
+      .startEvent()
+      .userTask("task1")
+      .endEvent()
+      .done();
+}

cibseven/cibseven

Signed-off-by: Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>

engine/src/test/java/org/cibseven/bpm/engine/test/history/JobEntityAndJobLogBatchIdTest.java
@@ -17,6 +17,7 @@
 package org.cibseven.bpm.engine.test.history;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.cibseven.bpm.engine.test.util.ExecutableProcessUtil.USER_TASK_PROCESS;
 
 import java.util.List;
 import java.util.Map;
@@ -83,14 +84,6 @@ public class JobEntityAndJobLogBatchIdTest {
     externalTaskService = engineRule.getExternalTaskService();
   }
 
-  private BpmnModelInstance getUserTaskProcess() {
-    return Bpmn.createExecutableProcess("process")
-        .startEvent()
-        .userTask("task1")
-        .endEvent()
-        .done();
-  }
-
   private BpmnModelInstance getTwoUserTasksProcess() {
     return Bpmn.createExecutableProcess("process")
         .startEvent()
@@ -111,7 +104,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_SetHistoricBatchRemovalTime() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
 
     // create historic Batch
@@ -159,7 +152,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_SetVariables() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.setVariablesAsync(List.of(process.getId()), Variables.createVariables().putValue("foo", "bar"));
@@ -250,7 +243,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_DeleteProcessInstances() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.deleteProcessInstancesAsync(List.of(process.getId()), null);
@@ -289,7 +282,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_Migration() {
     // given
-    ProcessDefinition sourceProcessDefinition = testRule.deployAndGetDefinition(getUserTaskProcess());
+    ProcessDefinition sourceProcessDefinition = testRule.deployAndGetDefinition(USER_TASK_PROCESS);
     ProcessInstance process = runtimeService.startProcessInstanceByKey("process");
     ProcessDefinition targetProcessDefinition = testRule.deployAndGetDefinition(getTwoUserTasksProcess());
 
@@ -351,7 +344,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_RestartProcessInstance() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
     runtimeService.deleteProcessInstance(processInstance.getId(), null);
 
@@ -411,7 +404,7 @@ public class JobEntityAndJobLogBatchIdTest {
   @Test
   public void shouldSetBatchIdOnJobAndJobLog_UpdateProcessInstancesSuspendState() {
     // given
-    testRule.deploy(getUserTaskProcess());
+    testRule.deploy(USER_TASK_PROCESS);
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
 
     Batch batch = runtimeService.updateProcessInstanceSuspensionState()

engine/src/test/java/org/cibseven/bpm/engine/test/util/ExecutableProcessUtil.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.engine.test.util;
+
+import org.cibseven.bpm.model.bpmn.Bpmn;
+import org.cibseven.bpm.model.bpmn.BpmnModelInstance;
+
+public class ExecutableProcessUtil {
+
+  public static final BpmnModelInstance USER_TASK_PROCESS = Bpmn.createExecutableProcess("process")
+      .startEvent()
+      .userTask("task1")
+      .endEvent()
+      .done();
+}