3f5e181030cdce4d563706c83ed702a4bf78c08c
Serge Krot <serge.krot@cib.de>
2024-12-18 10:26:42 +0100

    chore(tests) JacksonConfigurator: added more use cases

--- engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/mapper/JacksonConfiguratorTest.java
+++ engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/mapper/JacksonConfiguratorTest.java
@@ -10,21 +10,62 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonConfiguratorTest {
 
+    final static int STD_LIMIT = StreamReadConstraints.DEFAULT_MAX_STRING_LEN;
+	final static int ACTUAL_LIMIT = 50_000_000;
+
 	@Test
-	public void testMaxStringLength() throws JsonProcessingException {
+	public void testMaxStringLengthBelow() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT - 1);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testMaxStringLengthExactly() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testMaxStringLengthAbove() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT + 1);
+		assertEquals(status, false);
+	}
+
+    @Test
+	public void testStdStringLengthBelow() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT - 1);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testStdStringLengthExactly() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testStdStringLengthAbove() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT + 1);
+		assertEquals(status, true);
+	}
+
+	public boolean test(final int dataLength) throws JsonProcessingException {
 
 		JacksonConfigurator configurator = new JacksonConfigurator();
 		ObjectMapper objectMapper = configurator.getContext(ObjectMapper.class);
 
-	        int limit = 50_000_000;
-
 		StreamReadConstraints constraints = objectMapper.getFactory().streamReadConstraints();
-		assertEquals(limit, constraints.getMaxStringLength());
+		assertEquals(ACTUAL_LIMIT, constraints.getMaxStringLength());
 
-		String longString = new String(new char[limit]).replace('\0', 'x');
-		String json = objectMapper.writeValueAsString(longString);
-		String deserializedString = objectMapper.readValue(json, String.class);
-		assertEquals(longString, deserializedString);
+		try {
+			String longString = new String(new char[dataLength]).replace('\0', 'x');
+			String json = objectMapper.writeValueAsString(longString);
+			String deserializedString = objectMapper.readValue(json, String.class);
+			assertEquals(longString, deserializedString);
+			return true;
+		}
+		catch (Exception ex) {
+			return false;
+		}
 	}
-
 }
