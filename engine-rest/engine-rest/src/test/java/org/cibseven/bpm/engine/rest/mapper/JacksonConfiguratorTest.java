c7296100ac81b394cb6e10a9b37496fb46ef4603
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2024-12-18 10:28:28 +0100

    feat(engine-rest): Jackson data transfer limit made configurable (#29)
    * feat(engine-rest): CIB7-114 Jackson data transfer limit made configurable
    * using jackson default max string length
    * chore(tests) JacksonConfigurator: added more use cases
    
    Co-authored-by: Serge Krot <serge.krot@cib.de>

---
+++ engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/mapper/JacksonConfiguratorTest.java
@@ -0,0 +1,71 @@
+package org.cibseven.bpm.engine.rest.mapper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonConfiguratorTest {
+
+    final static int STD_LIMIT = StreamReadConstraints.DEFAULT_MAX_STRING_LEN;
+	final static int ACTUAL_LIMIT = 50_000_000;
+
+	@Test
+	public void testMaxStringLengthBelow() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT - 1);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testMaxStringLengthExactly() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testMaxStringLengthAbove() throws JsonProcessingException {
+		final boolean status = test(ACTUAL_LIMIT + 1);
+		assertEquals(status, false);
+	}
+
+    @Test
+	public void testStdStringLengthBelow() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT - 1);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testStdStringLengthExactly() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT);
+		assertEquals(status, true);
+	}
+
+	@Test
+	public void testStdStringLengthAbove() throws JsonProcessingException {
+		final boolean status = test(STD_LIMIT + 1);
+		assertEquals(status, true);
+	}
+
+	public boolean test(final int dataLength) throws JsonProcessingException {
+
+		JacksonConfigurator configurator = new JacksonConfigurator();
+		ObjectMapper objectMapper = configurator.getContext(ObjectMapper.class);
+
+		StreamReadConstraints constraints = objectMapper.getFactory().streamReadConstraints();
+		assertEquals(ACTUAL_LIMIT, constraints.getMaxStringLength());
+
+		try {
+			String longString = new String(new char[dataLength]).replace('\0', 'x');
+			String json = objectMapper.writeValueAsString(longString);
+			String deserializedString = objectMapper.readValue(json, String.class);
+			assertEquals(longString, deserializedString);
+			return true;
+		}
+		catch (Exception ex) {
+			return false;
+		}
+	}
+}
