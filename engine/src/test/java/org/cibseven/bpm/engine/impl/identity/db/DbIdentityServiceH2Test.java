7646af8afc6dd2be4dfa8497e16298cc48d8dbd0
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2024-12-18 10:55:17 +0100

    feat(engine): Login with internal database user made case-insensitive (#28)
    * feat(engine): CIB7-110 Login with internal database user made case-insensitive
    * using History Level Full, switched from @BeforeClass to @ClassRule
    * test restructured for better support further dbs and later maven profiles

---
+++ engine/src/test/java/org/cibseven/bpm/engine/impl/identity/db/DbIdentityServiceH2Test.java
@@ -0,0 +1,26 @@
+package org.cibseven.bpm.engine.impl.identity.db;
+
+import org.cibseven.bpm.engine.ProcessEngineConfiguration;
+import org.cibseven.bpm.engine.test.ProcessEngineRule;
+import org.junit.Rule;
+
+public class DbIdentityServiceH2Test extends DbIdentityServiceTestAbstract {
+
+	@Rule
+	public final ProcessEngineRule processEngineRule = new ProcessEngineRule(
+		ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
+//			.setJdbcUrl("jdbc:h2:mem:camunda;DB_CLOSE_DELAY=-1")
+//			.setJdbcDriver("org.h2.Driver")
+//			.setJdbcUsername("sa")
+//			.setJdbcPassword("")
+			.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+			.setHistory(ProcessEngineConfiguration.HISTORY_FULL)
+			.buildProcessEngine()
+	);
+	
+	@Override
+	protected ProcessEngineRule getProcessEngineRule() {
+		return processEngineRule;
+	}
+
+}
---
+++ engine/src/test/java/org/cibseven/bpm/engine/impl/identity/db/DbIdentityServiceTestAbstract.java
@@ -0,0 +1,75 @@
+package org.cibseven.bpm.engine.impl.identity.db;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.cibseven.bpm.engine.IdentityService;
+import org.cibseven.bpm.engine.impl.persistence.entity.UserEntity;
+import org.cibseven.bpm.engine.test.ProcessEngineRule;
+import org.junit.Test;
+
+public abstract class DbIdentityServiceTestAbstract {
+
+	protected abstract ProcessEngineRule getProcessEngineRule();
+	
+	@Test
+	public void testCheckPasswordCaseInsensitiveUserId() {
+		checkPasswordCaseInsensitiveUserId(getProcessEngineRule().getIdentityService());
+	}
+
+	private void checkPasswordCaseInsensitiveUserId(IdentityService identityService) {
+		// Create a test user
+		UserEntity user = new UserEntity();
+		String userId = "testuser";
+		user.setId(userId);
+		user.setPassword("s3cret");
+		identityService.saveUser(user);
+
+		try {
+			// Test with different cases for userId
+			assertTrue(identityService.checkPassword("TESTUSER", "s3cret"));
+			assertTrue(identityService.checkPassword("testuser", "s3cret"));
+			assertTrue(identityService.checkPassword("TestUser", "s3cret"));
+			assertFalse(identityService.checkPassword("testuser", "wrongpassword"));
+			assertFalse(identityService.checkPassword("wronguser", "s3cret"));
+		} finally {
+			identityService.deleteUser(userId);
+		}
+	}
+	
+	@Test
+	public void testCheckPasswordWithSimilarUserIds() {
+		checkPasswordWithSimilarUserIds(getProcessEngineRule().getIdentityService());
+	}
+
+	private void checkPasswordWithSimilarUserIds(IdentityService identityService) {
+		
+		UserEntity user1 = new UserEntity();
+		String userId1 = "TestUser";
+		user1.setId(userId1);
+		user1.setPassword("password1");
+		identityService.saveUser(user1);
+
+		UserEntity user2 = new UserEntity();
+		String userId2 = "TESTUSER";
+		user2.setId(userId2);
+		user2.setPassword("password2");
+		identityService.saveUser(user2);
+
+		try {
+			assertTrue(identityService.checkPassword("TestUser", "password1"));
+			assertTrue(identityService.checkPassword("TESTUSER", "password2"));
+			
+			assertTrue(identityService.checkPassword("testuser", "password1"));
+			// first returned user has password1
+			assertFalse(identityService.checkPassword("testuser", "password2"));
+			
+			assertFalse(identityService.checkPassword("TestUser", "wrongpassword"));
+			assertFalse(identityService.checkPassword("TESTUSER", "wrongpassword"));
+		} finally {
+			identityService.deleteUser(userId1);
+			identityService.deleteUser(userId2);
+		}
+	}
+	
+}
