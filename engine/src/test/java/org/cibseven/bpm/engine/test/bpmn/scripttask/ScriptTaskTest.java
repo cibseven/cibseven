50f26a8deba1090eb8c249e3277e1e2495149657
vladimirg <vladimir.gribko@cib.de>
2025-03-20 16:13:51 +0300

    Use common script test class again to avoid new files

--- engine/src/test/java/org/cibseven/bpm/engine/test/bpmn/scripttask/ScriptTaskTest.java
+++ engine/src/test/java/org/cibseven/bpm/engine/test/bpmn/scripttask/ScriptTaskTest.java
@@ -18,6 +18,8 @@ package org.cibseven.bpm.engine.test.bpmn.scripttask;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.cibseven.bpm.engine.impl.scripting.engine.CamundaScriptEngineManager.CAMUNDA_NAMESPACE;
+import static org.cibseven.bpm.engine.impl.scripting.engine.CamundaScriptEngineManager.CIBSEVEN_NAMESPACE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -26,11 +28,13 @@ import static org.junit.Assert.fail;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.cibseven.bpm.engine.ProcessEngineException;
 import org.cibseven.bpm.engine.ScriptCompilationException;
 import org.cibseven.bpm.engine.ScriptEvaluationException;
+import org.cibseven.bpm.engine.delegate.BpmnError;
 import org.cibseven.bpm.engine.exception.NullValueException;
 import org.cibseven.bpm.engine.impl.util.CollectionUtil;
 import org.cibseven.bpm.engine.repository.ProcessDefinition;
@@ -725,6 +729,73 @@ public class ScriptTaskTest extends AbstractScriptTaskTest {
     // this is not allowed in the JS ScriptEngine
       .isInstanceOf(ScriptEvaluationException.class)
       .hasMessageContaining("Operation is not allowed");
+  }
+  
+  @Test
+  public void shouldLoadCibSevenClassGroovy() {
+
+    // Save current configuration for using name-space and caching
+    boolean useCibSevenNamespace = processEngineConfiguration.isUseCibSevenNamespaceInScripting();
+    boolean useScriptEngineCaching = processEngineConfiguration.getScriptingEngines().isEnableScriptEngineCaching();
+    
+    // Disable caching to avoid using already cached engine and set name-space option 
+    processEngineConfiguration.getScriptingEngines().setEnableScriptEngineCaching(false);
+    processEngineConfiguration.setUseCibSevenNamespaceInScripting(true);
+    
+    
+    String cibsevenPackage = BpmnError.class.getPackageName();
+    String camundaPackage = cibsevenPackage.replace(CIBSEVEN_NAMESPACE, CAMUNDA_NAMESPACE);
+    String existingCommunityPackage = org.camunda.community.BpmnError.class.getPackageName();
+    String wrongPackage = "org.wrongpackage";
+
+    List<String[]> packages = List.of(
+        new String[] { camundaPackage, cibsevenPackage },
+        new String[] { cibsevenPackage, cibsevenPackage },
+        new String[] { existingCommunityPackage, existingCommunityPackage }, 
+        new String[] { wrongPackage, null } // should not be accessible
+    );
+
+    for (String[] tuple : packages) {
+
+      String processPackage = tuple[0];
+      String expectedPackage = tuple[1];
+
+      final String expectedClass = BpmnError.class.getSimpleName();
+      final String expectedMessage = "ServiceTaskError";
+
+      final String errorMessageVar = "errorMessage";
+      final String errorClassVar = "errorClass";
+      final String errorPackageVar = "errorPackage";
+
+      // Given
+      String scriptText = "try {\n" + "  def error = new " + processPackage + "." + expectedClass + "(\""
+          + expectedMessage + "\");\n" + "  def message = error.getErrorCode() ?: \"Default error code\";\n"
+          + "  execution.setVariable('" + errorClassVar + "', error.getClass().getName());\n"
+          + "  execution.setVariable('" + errorPackageVar + "', error.getClass().getPackage().getName());\n"
+          + "  execution.setVariable('" + errorMessageVar + "', message);\n"
+          + "} catch (Exception e) { execution.setVariable('" + errorMessageVar + "', e.message);}\n";
+
+      deployProcess(GROOVY, scriptText);
+
+      if (expectedPackage != null) {
+        
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("testProcess");
+        assertEquals(expectedMessage, runtimeService.getVariable(pi.getId(), errorMessageVar));
+        assertEquals(expectedPackage + "." + expectedClass, runtimeService.getVariable(pi.getId(), errorClassVar));
+        assertEquals(expectedPackage, runtimeService.getVariable(pi.getId(), errorPackageVar));
+      } else {
+        
+        // When
+        assertThatThrownBy(() -> runtimeService.startProcessInstanceByKey("testProcess"))
+            // Then
+            .isInstanceOf(ScriptCompilationException.class)
+            .hasMessageContaining("Unable to compile script");
+      }
+    }
+    
+    // Restore configuration
+    processEngineConfiguration.setUseCibSevenNamespaceInScripting(useCibSevenNamespace);
+    processEngineConfiguration.getScriptingEngines().setEnableScriptEngineCaching(useScriptEngineCaching);
   }
 
 }
