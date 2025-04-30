camunda/camunda-bpm-platform

feat(engine-plugins): Support FEEL Scala Engine (#5018)
related to: camunda/camunda-bpm-platform/issues/2384

* feat(engine-plugins): Support FEEL Scala Engine
* feat(feel-scala): register the FEEL JavaValueMapper  SPI
* feat(feel): configure wildfly distro
* exclude FeelEngineTest for wildfly26
* exclude FeelEngineTest for wildfly26-servlet
* exclude FeelEngineTest for wildfly & wildfly-servlet
* camunda-engine-plugin-spin should be optional

related to: camunda/camunda-bpm-platform/issues/2384
---------

Co-authored-by: Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
Co-authored-by: yanavasileva <yanavasileva@users.noreply.github.com>

qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/spin/FeelContextDelegate.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.integrationtest.functional.spin;
+
+import java.util.Map;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class FeelContextDelegate implements JavaDelegate {
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    Map<String, Object> context = (Map<String, Object>) execution.getVariable("context");
+    Map<String, Object> nestedMap = (Map<String, Object>) context.get("innerContext");
+
+    String content = (String) nestedMap.get("content");
+    execution.setVariable("result", content);
+  }
+}

qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/spin/FeelEngineTest.java
@@ -17,6 +17,7 @@
 package org.camunda.bpm.integrationtest.functional.spin;
 
 import org.camunda.bpm.engine.history.HistoricDecisionInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.integrationtest.util.AbstractFoxPlatformIntegrationTest;
@@ -32,9 +33,10 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
-public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
+public class FeelEngineTest extends AbstractFoxPlatformIntegrationTest {
 
   protected static final String PATH = "org/camunda/bpm/integrationtest/functional/spin/feel/";
 
@@ -42,6 +44,8 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
   protected static final String DMN_XML = "feel-spin-xml-decision.dmn";
   protected static final String PROCESS_JSON = "feel-spin-json-process.bpmn";
   protected static final String PROCESS_XML = "feel-spin-xml-process.bpmn";
+  protected static final String FEEL_EXCLUSIVE_GATEWAY_BMPN = "feel-spin-process.bpmn";
+  protected static final String FEEL_INPUT_OUTPUT_PROCESS_BPMN = "feel-spin-input-output.bpmn";
 
   protected static final List<String> TEST_LIST = Arrays.asList("\"foo\"", "\"bar\"");
 
@@ -53,6 +57,9 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
         .addAsResource(PATH + DMN_XML, DMN_XML)
         .addAsResource(PATH + PROCESS_JSON, PROCESS_JSON)
         .addAsResource(PATH + PROCESS_XML, PROCESS_XML)
+        .addAsResource(PATH + FEEL_EXCLUSIVE_GATEWAY_BMPN, FEEL_EXCLUSIVE_GATEWAY_BMPN)
+        .addAsResource(PATH + FEEL_INPUT_OUTPUT_PROCESS_BPMN, FEEL_INPUT_OUTPUT_PROCESS_BPMN)
+        .addClass(FeelContextDelegate.class)
         .addClass(JsonListSerializable.class)
         .addClass(XmlListSerializable.class)
         .addClass(AbstractFoxPlatformIntegrationTest.class);
@@ -92,6 +99,39 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
 
     assertThat(hdi.getOutputs().size(), is(1));
     assertThat(hdi.getOutputs().get(0).getValue(), is(true));
+  }
+
+  @Test
+  public void testSpinIntegration() {
+    // Accessing SPIN object from FEEL requires the org.camunda.spin.plugin.impl.feel.integration.SpinValueMapper SPI
+    // given
+    VariableMap variablesLarge = Variables.createVariables().putValue("amount", Spin.JSON("{\"value\": 25}"));
+    VariableMap variablesSmall = Variables.createVariables().putValue("amount", Spin.JSON("{\"value\": 2}"));
+
+    // when
+    ProcessInstance pi1 = runtimeService.startProcessInstanceByKey("feelScriptExecution", variablesLarge);
+    List<String> resultsLarge = runtimeService.getActiveActivityIds(pi1.getId());
+
+    ProcessInstance pi2 = runtimeService.startProcessInstanceByKey("feelScriptExecution", variablesSmall);
+    List<String> resultsSmall = runtimeService.getActiveActivityIds(pi2.getId());
+
+    // then
+    assertEquals(1, resultsLarge.size());
+    assertEquals("taskRequestInvoice", resultsLarge.get(0));
+
+    assertEquals(1, resultsSmall.size());
+    assertEquals("taskApprove", resultsSmall.get(0));
+  }
+
+  @Test
+  public void testFeelEngineComplexContext() {
+    // Mapping complex FEEL context into Java requires the org.camunda.feel.impl.JavaValueMapper SPI to be registered
+    // when
+    ProcessInstance pi = runtimeService.startProcessInstanceByKey("feelComplexContextProcess");
+    String result = (String) runtimeService.getVariable(pi.getId(), "result");
+
+    // then
+    assertEquals("contentFromInnerContext", result);
   }
 
   // HELPER

cibseven/cibseven

qa/integration-tests-engine/src/test/java/org/cibseven/bpm/integrationtest/functional/spin/FeelContextDelegate.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.integrationtest.functional.spin;
+
+import java.util.Map;
+import org.cibseven.bpm.engine.delegate.DelegateExecution;
+import org.cibseven.bpm.engine.delegate.JavaDelegate;
+
+public class FeelContextDelegate implements JavaDelegate {
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    Map<String, Object> context = (Map<String, Object>) execution.getVariable("context");
+    Map<String, Object> nestedMap = (Map<String, Object>) context.get("innerContext");
+
+    String content = (String) nestedMap.get("content");
+    execution.setVariable("result", content);
+  }
+}

qa/integration-tests-engine/src/test/java/org/cibseven/bpm/integrationtest/functional/spin/FeelEngineTest.java
@@ -17,6 +17,7 @@
 package org.cibseven.bpm.integrationtest.functional.spin;
 
 import org.cibseven.bpm.engine.history.HistoricDecisionInstance;
+import org.cibseven.bpm.engine.runtime.ProcessInstance;
 import org.cibseven.bpm.engine.variable.VariableMap;
 import org.cibseven.bpm.engine.variable.Variables;
 import org.cibseven.bpm.integrationtest.util.AbstractFoxPlatformIntegrationTest;
@@ -32,9 +33,10 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
-public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
+public class FeelEngineTest extends AbstractFoxPlatformIntegrationTest {
 
   protected static final String PATH = "org/cibseven/bpm/integrationtest/functional/spin/feel/";
 
@@ -42,6 +44,8 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
   protected static final String DMN_XML = "feel-spin-xml-decision.dmn";
   protected static final String PROCESS_JSON = "feel-spin-json-process.bpmn";
   protected static final String PROCESS_XML = "feel-spin-xml-process.bpmn";
+  protected static final String FEEL_EXCLUSIVE_GATEWAY_BMPN = "feel-spin-process.bpmn";
+  protected static final String FEEL_INPUT_OUTPUT_PROCESS_BPMN = "feel-spin-input-output.bpmn";
 
   protected static final List<String> TEST_LIST = Arrays.asList("\"foo\"", "\"bar\"");
 
@@ -53,6 +57,9 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
         .addAsResource(PATH + DMN_XML, DMN_XML)
         .addAsResource(PATH + PROCESS_JSON, PROCESS_JSON)
         .addAsResource(PATH + PROCESS_XML, PROCESS_XML)
+        .addAsResource(PATH + FEEL_EXCLUSIVE_GATEWAY_BMPN, FEEL_EXCLUSIVE_GATEWAY_BMPN)
+        .addAsResource(PATH + FEEL_INPUT_OUTPUT_PROCESS_BPMN, FEEL_INPUT_OUTPUT_PROCESS_BPMN)
+        .addClass(FeelContextDelegate.class)
         .addClass(JsonListSerializable.class)
         .addClass(XmlListSerializable.class)
         .addClass(AbstractFoxPlatformIntegrationTest.class);
@@ -92,6 +99,39 @@ public class FeelEngineIT extends AbstractFoxPlatformIntegrationTest {
 
     assertThat(hdi.getOutputs().size(), is(1));
     assertThat(hdi.getOutputs().get(0).getValue(), is(true));
+  }
+
+  @Test
+  public void testSpinIntegration() {
+    // Accessing SPIN object from FEEL requires the org.cibseven.spin.plugin.impl.feel.integration.SpinValueMapper SPI
+    // given
+    VariableMap variablesLarge = Variables.createVariables().putValue("amount", Spin.JSON("{\"value\": 25}"));
+    VariableMap variablesSmall = Variables.createVariables().putValue("amount", Spin.JSON("{\"value\": 2}"));
+
+    // when
+    ProcessInstance pi1 = runtimeService.startProcessInstanceByKey("feelScriptExecution", variablesLarge);
+    List<String> resultsLarge = runtimeService.getActiveActivityIds(pi1.getId());
+
+    ProcessInstance pi2 = runtimeService.startProcessInstanceByKey("feelScriptExecution", variablesSmall);
+    List<String> resultsSmall = runtimeService.getActiveActivityIds(pi2.getId());
+
+    // then
+    assertEquals(1, resultsLarge.size());
+    assertEquals("taskRequestInvoice", resultsLarge.get(0));
+
+    assertEquals(1, resultsSmall.size());
+    assertEquals("taskApprove", resultsSmall.get(0));
+  }
+
+  @Test
+  public void testFeelEngineComplexContext() {
+    // Mapping complex FEEL context into Java requires the org.cibseven.feel.impl.JavaValueMapper SPI to be registered
+    // when
+    ProcessInstance pi = runtimeService.startProcessInstanceByKey("feelComplexContextProcess");
+    String result = (String) runtimeService.getVariable(pi.getId(), "result");
+
+    // then
+    assertEquals("contentFromInnerContext", result);
   }
 
   // HELPER