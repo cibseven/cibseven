d0fd31c57993a0dbbf316ece8e44de593c2bb059
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2025-05-05 22:45:19 +0200

    feat(start-cibseven.bat): add logic to create jwtSecret file if it doesn't exist

--- distro/wildfly/assembly/src/start-cibseven.bat
+++ distro/wildfly/assembly/src/start-cibseven.bat
@@ -2,6 +2,33 @@
 
 set "JBOSS_HOME=%CD%\server\wildfly-${version.wildfly}"
 
+setlocal EnableDelayedExpansion
+
+:: Define the file path
+set "FILE=%JBOSS_HOME%\modules\org\cibseven\config\main\cibseven-webclient.properties"
+
+:: Check if the file exists
+if not exist "%FILE%" (
+
+    :: Generate a 155-character alphanumeric random string
+    set "CHARS=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    set "RANDOM_STRING="
+
+    for /L %%i in (1,1,155) do (
+        set /A "IDX=!random! %% 62"
+        for %%C in (!IDX!) do set "RANDOM_STRING=!RANDOM_STRING!!CHARS:~%%C,1!"
+    )
+
+    :: Write to the file with a newline at the end
+    > "%FILE%" echo.authentication.jwtSecret=!RANDOM_STRING!
+
+    echo File "%FILE%" created with random jwtSecret.
+) else (
+    echo File "%FILE%" already exists. No changes made.
+)
+
+endlocal
+
 echo "starting CIB seven ${project.version} on Wildfly Application Server ${version.wildfly}"
 
 cd server\wildfly-${version.wildfly}\bin\
