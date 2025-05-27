641196853524846385ac4100f96d8aab9ec25263
Oliver Specht <oliver.specht@cib.de>
2025-05-27 08:37:26 +0200

    Search for jwtSecret in environment first
    related to https://github.com/cibseven/cibseven/issues/93

---
+++ engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/security/auth/impl/ConfigurationTest.java
@@ -0,0 +1,24 @@
+package org.cibseven.bpm.engine.rest.security.auth.impl;
+
+import org.cibseven.bpm.engine.rest.security.auth.impl.jwt.Configuration;
+import org.junit.Assert;
+import org.junit.Test;
+
+import uk.org.webcompere.systemstubs.rules.EnvironmentVariablesRule;
+
+import org.junit.Rule;
+import org.cibseven.bpm.engine.rest.helper.MockProvider;
+
+public class ConfigurationTest {
+	
+	@Rule
+	public EnvironmentVariablesRule environmentVariablesRule =
+	  new EnvironmentVariablesRule("CIBSEVEN_WEBCLIENT_AUTHENTICATION_JWTSECRET", MockProvider.EXAMPLE_CONFIGURATION_JWTSECRET_ENVIRONMENT);
+	
+	@Test
+	public void testConfigurationWithEnvironment() {
+        String jwtSecret = Configuration.getInstance().getSecret();
+	    Assert.assertEquals(MockProvider.EXAMPLE_CONFIGURATION_JWTSECRET_ENVIRONMENT, jwtSecret);
+	}
+
+}
---
+++ engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/security/auth/impl/ConfigurationTestNoEnv.java
@@ -0,0 +1,16 @@
+package org.cibseven.bpm.engine.rest.security.auth.impl;
+
+import org.cibseven.bpm.engine.rest.security.auth.impl.jwt.Configuration;
+import org.junit.Assert;
+import org.junit.Test;
+import org.cibseven.bpm.engine.rest.helper.MockProvider;
+
+public class ConfigurationTestNoEnv {
+	
+	@Test
+	public void testConfigurationFromProperties() {
+	    String jwtSecret = Configuration.getInstance().getSecret();
+	    Assert.assertEquals(MockProvider.EXAMPLE_CONFIGURATION_JWTSECRET_PROPERTY, jwtSecret);
+	}
+
+}
