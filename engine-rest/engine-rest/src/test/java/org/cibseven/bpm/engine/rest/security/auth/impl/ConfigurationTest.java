8dfbc7b1ca3f44cec39ea11eca6d29f2ed71bb10
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2025-06-24 13:28:24 +0200

    system-stubs-junit4 updated to the latest version

--- engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/security/auth/impl/ConfigurationTest.java
+++ engine-rest/engine-rest/src/test/java/org/cibseven/bpm/engine/rest/security/auth/impl/ConfigurationTest.java
@@ -2,7 +2,6 @@ package org.cibseven.bpm.engine.rest.security.auth.impl;
 
 import org.cibseven.bpm.engine.rest.security.auth.impl.jwt.Configuration;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.org.webcompere.systemstubs.rules.EnvironmentVariablesRule;
@@ -16,7 +15,6 @@ public class ConfigurationTest {
 	public EnvironmentVariablesRule environmentVariablesRule =
 	  new EnvironmentVariablesRule("CIBSEVEN_WEBCLIENT_AUTHENTICATION_JWTSECRET", MockProvider.EXAMPLE_CONFIGURATION_JWTSECRET_ENVIRONMENT);
 	
-	@Ignore
 	@Test
 	public void testConfigurationWithEnvironment() {
         Assert.assertEquals(System.getenv("CIBSEVEN_WEBCLIENT_AUTHENTICATION_JWTSECRET"), MockProvider.EXAMPLE_CONFIGURATION_JWTSECRET_ENVIRONMENT);
