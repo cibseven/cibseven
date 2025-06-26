camunda/camunda-bpm-platform

feat(api): filter process instances by rootProcessInstanceId
* Impl for rootProcessInstanceId for PI Updated with Master

* remove autoformat issues only

* update to format only

engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/dto/history/HistoricProcessInstanceQueryDto.java
@@ -74,6 +74,7 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
   private String processInstanceId;
   private Set<String> processInstanceIds;
   private List<String> processInstanceIdNotIn;
+  private String rootProcessInstanceId;
   private String processDefinitionId;
   private String processDefinitionKey;
   private List<String> processDefinitionKeys;
@@ -150,6 +151,15 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
   @CamundaQueryParam(value = "processInstanceIdNotIn", converter = StringListConverter.class)
   public void setProcessInstanceIdNotIn(List<String> processInstanceIdNotIn) {
     this.processInstanceIdNotIn = processInstanceIdNotIn;
+  }
+
+  public String getRootProcessInstanceId() {
+    return rootProcessInstanceId;
+  }
+
+  @CamundaQueryParam("rootProcessInstanceId")
+  public void setRootProcessInstanceId(String rootProcessInstanceId) {
+    this.rootProcessInstanceId = rootProcessInstanceId;
   }
 
   public String getProcessDefinitionId() {
@@ -427,6 +437,9 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
     }
     if (processInstanceIdNotIn != null && !processInstanceIdNotIn.isEmpty()) {
       query.processInstanceIdNotIn(processInstanceIdNotIn.toArray(new String[0]));
+    }
+    if (rootProcessInstanceId != null) {
+      query.rootProcessInstanceId(rootProcessInstanceId);
     }
     if (processDefinitionId != null) {
       query.processDefinitionId(processDefinitionId);

engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/dto/runtime/ProcessInstanceQueryDto.java
@@ -75,6 +75,7 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
   private Set<String> processInstanceIds;
   private Boolean withIncident;
   private String incidentId;
+  private String rootProcessInstanceId;
   private String incidentType;
   private String incidentMessage;
   private String incidentMessageLike;
@@ -285,6 +286,15 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
     this.incidentId = incidentId;
   }
 
+  public String getRootProcessInstanceId() {
+    return rootProcessInstanceId;
+  }
+
+  @CamundaQueryParam("rootProcessInstanceId")
+  public void setRootProcessInstanceId(String rootProcessInstanceId) {
+    this.rootProcessInstanceId = rootProcessInstanceId;
+  }
+
   public String getIncidentType() {
     return incidentType;
   }
@@ -441,6 +451,9 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
     }
     if (incidentId != null) {
       query.incidentId(incidentId);
+    }
+    if (rootProcessInstanceId != null) {
+      query.rootProcessInstanceId(rootProcessInstanceId);
     }
     if (incidentType != null) {
       query.incidentType(incidentType);

engine/src/test/java/org/camunda/bpm/engine/test/api/runtime/ProcessInstanceQueryTest.java
@@ -62,6 +62,7 @@ import org.camunda.bpm.engine.test.api.runtime.migration.models.ProcessModels;
 import org.camunda.bpm.engine.test.util.ProcessEngineTestRule;
 import org.camunda.bpm.engine.test.util.ProvidedProcessEngineRule;
 import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.Assert;
 import org.junit.Before;
@@ -2225,6 +2226,45 @@ public class ProcessInstanceQueryTest {
     List<ProcessInstance> list = query.list();
     assertEquals(1, list.size());
     assertEquals(processInstanceId, list.get(0).getId());
+  }
+
+  @Test
+  public void testQueryByRootProcessInstanceId() {
+    // given a parent process instance with a couple of subprocesses
+    BpmnModelInstance baseProcess = Bpmn.createExecutableProcess("baseProcess")
+        .startEvent("startRoot")
+        .callActivity()
+        .calledElement("levelOneProcess")
+        .endEvent("endRoot")
+        .done();
+
+    BpmnModelInstance levelOneProcess = Bpmn.createExecutableProcess("levelOneProcess")
+        .startEvent("startLevelOne")
+        .callActivity()
+        .calledElement("levelTwoProcess")
+        .endEvent("endLevelOne")
+        .done();
+
+    BpmnModelInstance levelTwoProcess = Bpmn.createExecutableProcess("levelTwoProcess")
+        .startEvent("startLevelTwo")
+        .userTask("Task1")
+        .endEvent("endLevelTwo")
+        .done();
+
+    testHelper.deploy(baseProcess, levelOneProcess, levelTwoProcess);
+    String rootProcessId = runtimeService.startProcessInstanceByKey("baseProcess").getId();
+    List<ProcessInstance> allProcessInstances = runtimeService.createProcessInstanceQuery().list();
+
+    // when
+    ProcessInstanceQuery processInstanceQuery = runtimeService.createProcessInstanceQuery()
+        .rootProcessInstanceId(rootProcessId);
+
+    // then
+    assertEquals(3, allProcessInstances.stream()
+        .filter(process -> process.getRootProcessInstanceId().equals(rootProcessId))
+        .count());
+    assertEquals(3, processInstanceQuery.count());
+    assertEquals(3, processInstanceQuery.list().size());
   }
 
   @Test

cibseven/cibseven

engine-rest/engine-rest/src/main/java/org/cibseven/bpm/engine/rest/dto/history/HistoricProcessInstanceQueryDto.java
@@ -74,6 +74,7 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
   private String processInstanceId;
   private Set<String> processInstanceIds;
   private List<String> processInstanceIdNotIn;
+  private String rootProcessInstanceId;
   private String processDefinitionId;
   private String processDefinitionKey;
   private List<String> processDefinitionKeys;
@@ -150,6 +151,15 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
   @CamundaQueryParam(value = "processInstanceIdNotIn", converter = StringListConverter.class)
   public void setProcessInstanceIdNotIn(List<String> processInstanceIdNotIn) {
     this.processInstanceIdNotIn = processInstanceIdNotIn;
+  }
+
+  public String getRootProcessInstanceId() {
+    return rootProcessInstanceId;
+  }
+
+  @CibsevenQueryParam("rootProcessInstanceId")
+  public void setRootProcessInstanceId(String rootProcessInstanceId) {
+    this.rootProcessInstanceId = rootProcessInstanceId;
   }
 
   public String getProcessDefinitionId() {
@@ -427,6 +437,9 @@ public class HistoricProcessInstanceQueryDto extends AbstractQueryDto<HistoricPr
     }
     if (processInstanceIdNotIn != null && !processInstanceIdNotIn.isEmpty()) {
       query.processInstanceIdNotIn(processInstanceIdNotIn.toArray(new String[0]));
+    }
+    if (rootProcessInstanceId != null) {
+      query.rootProcessInstanceId(rootProcessInstanceId);
     }
     if (processDefinitionId != null) {
       query.processDefinitionId(processDefinitionId);

engine-rest/engine-rest/src/main/java/org/cibseven/bpm/engine/rest/dto/runtime/ProcessInstanceQueryDto.java
@@ -75,6 +75,7 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
   private Set<String> processInstanceIds;
   private Boolean withIncident;
   private String incidentId;
+  private String rootProcessInstanceId;
   private String incidentType;
   private String incidentMessage;
   private String incidentMessageLike;
@@ -285,6 +286,15 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
     this.incidentId = incidentId;
   }
 
+  public String getRootProcessInstanceId() {
+    return rootProcessInstanceId;
+  }
+
+  @CibsevenQueryParam("rootProcessInstanceId")
+  public void setRootProcessInstanceId(String rootProcessInstanceId) {
+    this.rootProcessInstanceId = rootProcessInstanceId;
+  }
+
   public String getIncidentType() {
     return incidentType;
   }
@@ -441,6 +451,9 @@ public class ProcessInstanceQueryDto extends AbstractQueryDto<ProcessInstanceQue
     }
     if (incidentId != null) {
       query.incidentId(incidentId);
+    }
+    if (rootProcessInstanceId != null) {
+      query.rootProcessInstanceId(rootProcessInstanceId);
     }
     if (incidentType != null) {
       query.incidentType(incidentType);

engine/src/test/java/org/cibseven/bpm/engine/test/api/runtime/ProcessInstanceQueryTest.java
@@ -62,6 +62,7 @@ import org.cibseven.bpm.engine.test.api.runtime.migration.models.ProcessModels;
 import org.cibseven.bpm.engine.test.util.ProcessEngineTestRule;
 import org.cibseven.bpm.engine.test.util.ProvidedProcessEngineRule;
 import org.cibseven.bpm.engine.variable.Variables;
+import org.cibseven.bpm.model.bpmn.Bpmn;
 import org.cibseven.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.Assert;
 import org.junit.Before;
@@ -2225,6 +2226,45 @@ public class ProcessInstanceQueryTest {
     List<ProcessInstance> list = query.list();
     assertEquals(1, list.size());
     assertEquals(processInstanceId, list.get(0).getId());
+  }
+
+  @Test
+  public void testQueryByRootProcessInstanceId() {
+    // given a parent process instance with a couple of subprocesses
+    BpmnModelInstance baseProcess = Bpmn.createExecutableProcess("baseProcess")
+        .startEvent("startRoot")
+        .callActivity()
+        .calledElement("levelOneProcess")
+        .endEvent("endRoot")
+        .done();
+
+    BpmnModelInstance levelOneProcess = Bpmn.createExecutableProcess("levelOneProcess")
+        .startEvent("startLevelOne")
+        .callActivity()
+        .calledElement("levelTwoProcess")
+        .endEvent("endLevelOne")
+        .done();
+
+    BpmnModelInstance levelTwoProcess = Bpmn.createExecutableProcess("levelTwoProcess")
+        .startEvent("startLevelTwo")
+        .userTask("Task1")
+        .endEvent("endLevelTwo")
+        .done();
+
+    testHelper.deploy(baseProcess, levelOneProcess, levelTwoProcess);
+    String rootProcessId = runtimeService.startProcessInstanceByKey("baseProcess").getId();
+    List<ProcessInstance> allProcessInstances = runtimeService.createProcessInstanceQuery().list();
+
+    // when
+    ProcessInstanceQuery processInstanceQuery = runtimeService.createProcessInstanceQuery()
+        .rootProcessInstanceId(rootProcessId);
+
+    // then
+    assertEquals(3, allProcessInstances.stream()
+        .filter(process -> process.getRootProcessInstanceId().equals(rootProcessId))
+        .count());
+    assertEquals(3, processInstanceQuery.count());
+    assertEquals(3, processInstanceQuery.list().size());
   }
 
   @Test