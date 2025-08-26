2b7be75933b2d612e5784e3bf7d215652166b28d
Oliver Specht <oliver.specht@cib.de>
2025-08-26 08:57:04 +0200

    chore: Replace deprecated assert.* methods

--- engine-plugins/connect-plugin/src/test/java/org/cibseven/connect/plugin/ConnectProcessEnginePluginTest.java
+++ engine-plugins/connect-plugin/src/test/java/org/cibseven/connect/plugin/ConnectProcessEnginePluginTest.java
@@ -18,7 +18,7 @@ package org.cibseven.connect.plugin;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
