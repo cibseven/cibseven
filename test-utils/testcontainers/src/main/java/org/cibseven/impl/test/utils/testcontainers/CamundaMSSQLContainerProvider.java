daf63d48167d731b76761e40aa3db766f158fd25
vladimirg <vladimir.gribko@cib.de>
2025-10-15 21:08:15 +0200

    update mssql

--- test-utils/testcontainers/src/main/java/org/cibseven/impl/test/utils/testcontainers/CamundaMSSQLContainerProvider.java
+++ test-utils/testcontainers/src/main/java/org/cibseven/impl/test/utils/testcontainers/CamundaMSSQLContainerProvider.java
@@ -37,7 +37,7 @@ public class CamundaMSSQLContainerProvider extends MSSQLServerContainerProvider 
 
     MSSQLServerContainer mssqlServerContainer = new MSSQLServerContainer(dockerImageName);
     mssqlServerContainer.acceptLicense();	
-    mssqlServerContainer.setCommand("/opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P \"StrongP@ssw0rd\" -Q \"CREATE DATABASE process-engine; GO; ALTER DATABASE process-engine SET READ_COMMITTED_SNAPSHOT ON; GO; ALTER LOGIN sa WITH DEFAULT_DATABASE = process-engine; GO;\"");
+    mssqlServerContainer.setCommand("/opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -C -S localhost -U sa -P \"StrongP@ssw0rd\" -Q \"CREATE DATABASE process-engine; GO; ALTER DATABASE process-engine SET READ_COMMITTED_SNAPSHOT ON; GO; ALTER LOGIN sa WITH DEFAULT_DATABASE = process-engine; GO;\"");
 
     return mssqlServerContainer;
   }
