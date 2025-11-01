3bf206922f50f979eb5384b086d70ebdf1bcb50c
Oleg Skrypnyuk <oleg.skrypnyuk@cib.de>
2025-11-01 13:20:22 +0100

    fix(tests): update import statements in HistoricDecisionInstanceQueryTest

--- engine/src/test/java/org/cibseven/bpm/engine/test/history/dmn/HistoricDecisionInstanceQueryTest.java
+++ engine/src/test/java/org/cibseven/bpm/engine/test/history/dmn/HistoricDecisionInstanceQueryTest.java
@@ -17,9 +17,9 @@
 package org.cibseven.bpm.engine.test.history.dmn;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.camunda.bpm.engine.test.api.runtime.TestOrderingUtil.inverted;
-import static org.camunda.bpm.engine.test.api.runtime.TestOrderingUtil.propertyComparator;
-import static org.camunda.bpm.engine.test.api.runtime.TestOrderingUtil.verifySorting;
+import static org.cibseven.bpm.engine.test.api.runtime.TestOrderingUtil.inverted;
+import static org.cibseven.bpm.engine.test.api.runtime.TestOrderingUtil.propertyComparator;
+import static org.cibseven.bpm.engine.test.api.runtime.TestOrderingUtil.verifySorting;
 import static org.junit.Assert.fail;
 
 import java.util.Date;
