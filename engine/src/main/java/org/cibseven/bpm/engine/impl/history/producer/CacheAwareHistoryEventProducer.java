camunda/camunda-bpm-platform

feat(engine): adds process definition data to history events (#5155)
https://github.com/camunda/camunda-bpm-platform/issues/5291

engine/src/main/java/org/camunda/bpm/engine/impl/history/producer/CacheAwareHistoryEventProducer.java
@@ -23,7 +23,9 @@ import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.db.entitymanager.DbEntityManager;
 import org.camunda.bpm.engine.impl.history.event.*;
 import org.camunda.bpm.engine.impl.history.handler.DbHistoryEventHandler;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.camunda.bpm.engine.runtime.Incident;
 
 /**
@@ -112,6 +114,18 @@ public class CacheAwareHistoryEventProducer extends DefaultHistoryEventProducer 
     return Context.getCommandContext()
       .getDbEntityManager()
       .getCachedEntity(type, id);
+  }
+
+  @Override
+  protected ProcessDefinitionEntity getProcessDefinitionEntity(String processDefinitionId) {
+    CommandContext commandContext = Context.getCommandContext();
+    ProcessDefinitionEntity entity = null;
+    if(commandContext != null) {
+      entity = commandContext
+          .getDbEntityManager()
+          .getCachedEntity(ProcessDefinitionEntity.class, processDefinitionId);
+    }
+    return (entity != null) ? entity : super.getProcessDefinitionEntity(processDefinitionId);
   }
 
 }

engine/src/test/java/org/camunda/bpm/engine/test/history/HistoryEventDataTest.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.history;
+
+import static org.camunda.bpm.engine.impl.util.StringUtil.hasText;
+
+import java.util.Objects;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.RequiredHistoryLevel;
+import org.camunda.bpm.engine.test.util.ProcessEngineBootstrapRule;
+import org.camunda.bpm.engine.test.util.ProvidedProcessEngineRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Edoardo Patti
+ */
+@RequiredHistoryLevel(ProcessEngineConfiguration.HISTORY_FULL)
+public class HistoryEventDataTest {
+
+  private static final TestEventHandler HANDLER = new TestEventHandler();
+
+  @Rule
+  public HistoryEventVerifier verifier = new HistoryEventVerifier(HANDLER);
+
+  @ClassRule
+  public static ProcessEngineBootstrapRule bootstrapRule = new ProcessEngineBootstrapRule(
+      c -> c.setHistoryEventHandler(HANDLER));
+
+  private RuntimeService runtimeService;
+
+  @Rule
+  public ProvidedProcessEngineRule engineRule = new ProvidedProcessEngineRule(bootstrapRule);
+
+  @Before
+  public void initServices() {
+    runtimeService = engineRule.getRuntimeService();
+
+    verifier.historyEventIs("!= null", Objects::nonNull);
+    verifier.historyEventHas("processDefinitionId != null", (evt) -> hasText(evt.getProcessDefinitionId()));
+    verifier.historyEventHas("processDefinitionKey != null", (evt) -> hasText(evt.getProcessDefinitionKey()));
+    verifier.historyEventHas("processDefinitionName != null", (evt) -> hasText(evt.getProcessDefinitionName()));
+    verifier.historyEventHas("processDefinitionVersion != null", (evt) -> evt.getProcessDefinitionVersion() != null);
+  }
+
+  @Test
+  @Deployment(resources = "org/camunda/bpm/engine/test/api/threeTasksProcess.bpmn20.xml")
+  public void verify() {
+    runtimeService.startProcessInstanceByKey("threeTasksProcess");
+  }
+}

engine/src/test/java/org/camunda/bpm/engine/test/history/HistoryEventVerifier.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.history;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import org.assertj.core.api.Condition;
+import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
+import org.jetbrains.annotations.NotNull;
+import org.junit.rules.Verifier;
+
+/**
+ * @author Edoardo Patti
+ */
+public class HistoryEventVerifier extends Verifier {
+
+  private final TestEventHandler eventHandler;
+  private final List<Condition<HistoryEvent>> hasConditions = new ArrayList<>(50);
+  private final List<Condition<HistoryEvent>> isConditions = new ArrayList<>(50);
+
+
+  public HistoryEventVerifier(TestEventHandler eventHandler) {
+    this.eventHandler = eventHandler;
+  }
+
+  public void historyEventHas(String message, Predicate<HistoryEvent> predicate) {
+    Condition<HistoryEvent> hasCondition = getCondition(message, predicate);
+    this.hasConditions.add(hasCondition);
+  }
+
+  public void historyEventIs(String message, Predicate<HistoryEvent> predicate) {
+    Condition<HistoryEvent> isCondition = getCondition(message, predicate);
+    this.isConditions.add(isCondition);
+  }
+
+  @NotNull
+  private static Condition<HistoryEvent> getCondition(String message, Predicate<HistoryEvent> predicate) {
+
+    return new Condition<>(message) {
+
+      @Override
+      public boolean matches(HistoryEvent value) {
+        return predicate.test(value);
+      }
+    };
+  }
+
+  @Override
+  protected void verify() throws Throwable {
+    while (this.eventHandler.peek() != null) {
+      final HistoryEvent evt = this.eventHandler.poll();
+      hasConditions.forEach(condition -> assertThat(evt).has(condition));
+      isConditions.forEach(condition -> assertThat(evt).is(condition));
+    }
+  }
+}

engine/src/test/java/org/camunda/bpm/engine/test/history/TestEventHandler.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.history;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
+import org.camunda.bpm.engine.impl.history.handler.HistoryEventHandler;
+
+/**
+ * @author Edoardo Patti
+ */
+public class TestEventHandler implements HistoryEventHandler {
+
+  private final Queue<HistoryEvent> queue = new ArrayDeque<>(50);
+
+  @Override
+  public void handleEvent(HistoryEvent historyEvent) {
+    queue.offer(historyEvent);
+  }
+
+  @Override
+  public void handleEvents(List<HistoryEvent> historyEvents) {
+    historyEvents.forEach(queue::offer);
+  }
+
+  public HistoryEvent poll() {
+    return this.queue.poll();
+  }
+
+  public HistoryEvent peek() {
+    return this.queue.peek();
+  }
+}

cibseven/cibseven

engine/src/main/java/org/cibseven/bpm/engine/impl/history/producer/CacheAwareHistoryEventProducer.java
@@ -23,7 +23,9 @@ import org.cibseven.bpm.engine.impl.context.Context;
 import org.cibseven.bpm.engine.impl.db.entitymanager.DbEntityManager;
 import org.cibseven.bpm.engine.impl.history.event.*;
 import org.cibseven.bpm.engine.impl.history.handler.DbHistoryEventHandler;
+import org.cibseven.bpm.engine.impl.interceptor.CommandContext;
 import org.cibseven.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.cibseven.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.cibseven.bpm.engine.runtime.Incident;
 
 /**
@@ -112,6 +114,18 @@ public class CacheAwareHistoryEventProducer extends DefaultHistoryEventProducer 
     return Context.getCommandContext()
       .getDbEntityManager()
       .getCachedEntity(type, id);
+  }
+
+  @Override
+  protected ProcessDefinitionEntity getProcessDefinitionEntity(String processDefinitionId) {
+    CommandContext commandContext = Context.getCommandContext();
+    ProcessDefinitionEntity entity = null;
+    if(commandContext != null) {
+      entity = commandContext
+          .getDbEntityManager()
+          .getCachedEntity(ProcessDefinitionEntity.class, processDefinitionId);
+    }
+    return (entity != null) ? entity : super.getProcessDefinitionEntity(processDefinitionId);
   }
 
 }

engine/src/test/java/org/cibseven/bpm/engine/test/history/HistoryEventDataTest.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.engine.test.history;
+
+import static org.cibseven.bpm.engine.impl.util.StringUtil.hasText;
+
+import java.util.Objects;
+import org.cibseven.bpm.engine.ProcessEngineConfiguration;
+import org.cibseven.bpm.engine.RuntimeService;
+import org.cibseven.bpm.engine.test.Deployment;
+import org.cibseven.bpm.engine.test.RequiredHistoryLevel;
+import org.cibseven.bpm.engine.test.util.ProcessEngineBootstrapRule;
+import org.cibseven.bpm.engine.test.util.ProvidedProcessEngineRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Edoardo Patti
+ */
+@RequiredHistoryLevel(ProcessEngineConfiguration.HISTORY_FULL)
+public class HistoryEventDataTest {
+
+  private static final TestEventHandler HANDLER = new TestEventHandler();
+
+  @Rule
+  public HistoryEventVerifier verifier = new HistoryEventVerifier(HANDLER);
+
+  @ClassRule
+  public static ProcessEngineBootstrapRule bootstrapRule = new ProcessEngineBootstrapRule(
+      c -> c.setHistoryEventHandler(HANDLER));
+
+  private RuntimeService runtimeService;
+
+  @Rule
+  public ProvidedProcessEngineRule engineRule = new ProvidedProcessEngineRule(bootstrapRule);
+
+  @Before
+  public void initServices() {
+    runtimeService = engineRule.getRuntimeService();
+
+    verifier.historyEventIs("!= null", Objects::nonNull);
+    verifier.historyEventHas("processDefinitionId != null", (evt) -> hasText(evt.getProcessDefinitionId()));
+    verifier.historyEventHas("processDefinitionKey != null", (evt) -> hasText(evt.getProcessDefinitionKey()));
+    verifier.historyEventHas("processDefinitionName != null", (evt) -> hasText(evt.getProcessDefinitionName()));
+    verifier.historyEventHas("processDefinitionVersion != null", (evt) -> evt.getProcessDefinitionVersion() != null);
+  }
+
+  @Test
+  @Deployment(resources = "org/cibseven/bpm/engine/test/api/threeTasksProcess.bpmn20.xml")
+  public void verify() {
+    runtimeService.startProcessInstanceByKey("threeTasksProcess");
+  }
+}

engine/src/test/java/org/cibseven/bpm/engine/test/history/HistoryEventVerifier.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.engine.test.history;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import org.assertj.core.api.Condition;
+import org.cibseven.bpm.engine.impl.history.event.HistoryEvent;
+import org.jetbrains.annotations.NotNull;
+import org.junit.rules.Verifier;
+
+/**
+ * @author Edoardo Patti
+ */
+public class HistoryEventVerifier extends Verifier {
+
+  private final TestEventHandler eventHandler;
+  private final List<Condition<HistoryEvent>> hasConditions = new ArrayList<>(50);
+  private final List<Condition<HistoryEvent>> isConditions = new ArrayList<>(50);
+
+
+  public HistoryEventVerifier(TestEventHandler eventHandler) {
+    this.eventHandler = eventHandler;
+  }
+
+  public void historyEventHas(String message, Predicate<HistoryEvent> predicate) {
+    Condition<HistoryEvent> hasCondition = getCondition(message, predicate);
+    this.hasConditions.add(hasCondition);
+  }
+
+  public void historyEventIs(String message, Predicate<HistoryEvent> predicate) {
+    Condition<HistoryEvent> isCondition = getCondition(message, predicate);
+    this.isConditions.add(isCondition);
+  }
+
+  @NotNull
+  private static Condition<HistoryEvent> getCondition(String message, Predicate<HistoryEvent> predicate) {
+
+    return new Condition<>(message) {
+
+      @Override
+      public boolean matches(HistoryEvent value) {
+        return predicate.test(value);
+      }
+    };
+  }
+
+  @Override
+  protected void verify() throws Throwable {
+    while (this.eventHandler.peek() != null) {
+      final HistoryEvent evt = this.eventHandler.poll();
+      hasConditions.forEach(condition -> assertThat(evt).has(condition));
+      isConditions.forEach(condition -> assertThat(evt).is(condition));
+    }
+  }
+}

engine/src/test/java/org/cibseven/bpm/engine/test/history/TestEventHandler.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.engine.test.history;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import org.cibseven.bpm.engine.impl.history.event.HistoryEvent;
+import org.cibseven.bpm.engine.impl.history.handler.HistoryEventHandler;
+
+/**
+ * @author Edoardo Patti
+ */
+public class TestEventHandler implements HistoryEventHandler {
+
+  private final Queue<HistoryEvent> queue = new ArrayDeque<>(50);
+
+  @Override
+  public void handleEvent(HistoryEvent historyEvent) {
+    queue.offer(historyEvent);
+  }
+
+  @Override
+  public void handleEvents(List<HistoryEvent> historyEvents) {
+    historyEvents.forEach(queue::offer);
+  }
+
+  public HistoryEvent poll() {
+    return this.queue.poll();
+  }
+
+  public HistoryEvent peek() {
+    return this.queue.peek();
+  }
+}