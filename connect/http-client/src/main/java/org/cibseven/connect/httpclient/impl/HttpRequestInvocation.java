camunda/camunda-bpm-platform

feat(connect): upgrade to Apache HttpClient 5.x
Upgrade the HTTP and SOAP HTTP connectors from Apache HttpClient 4.x to 5.x:

**Dependency Changes:**
- Replace httpclient 4.5.14 with httpclient5 5.4.1
- Remove commons-codec dependency (no longer required)
- Consolidate version properties (httpclient5 → httpclient)

**Package Structure Migration:**
- Update all imports from org.apache.http.* to org.apache.hc.*
- Migrate from org.apache.http.client.* to org.apache.hc.client5.*
- Update core HTTP classes to org.apache.hc.core5.*

**API Modernization:**
- Replace CloseableHttpResponse with ClassicHttpResponse
- Replace HttpRequestBase with BasicClassicHttpRequest
- Replace HttpEntityEnclosingRequestBase with HttpUriRequestBase
- Update timeout configuration to use new Timeout API instead of int milliseconds
- Modernize entity handling with ContentType.parse()

**Configuration Updates:**
- Update RequestConfigOption enum for HttpClient 5.x compatibility
- Remove deprecated options (decompression-enabled, normalize-uri, etc.)
- Add new options (connection-keep-alive, hard-cancellation-enabled, response-timeout)
- Migrate from socket-timeout to response-timeout
- Update request config application logic

**Code Quality Improvements:**
- Add final modifiers to static fields and class members

related to camunda/camunda-bpm-platform#4438

connect/http-client/src/main/java/org/camunda/connect/httpclient/impl/HttpRequestInvocation.java
@@ -18,17 +18,17 @@ package org.camunda.connect.httpclient.impl;
 
 import java.util.List;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.camunda.connect.spi.ConnectorRequest;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.core5.http.message.BasicClassicHttpRequest;
 import org.camunda.connect.impl.AbstractRequestInvocation;
+import org.camunda.connect.spi.ConnectorRequest;
 import org.camunda.connect.spi.ConnectorRequestInterceptor;
 
-public class HttpRequestInvocation  extends AbstractRequestInvocation<HttpRequestBase> {
+public class HttpRequestInvocation extends AbstractRequestInvocation<BasicClassicHttpRequest> {
 
   protected HttpClient client;
 
-  public HttpRequestInvocation(HttpRequestBase target, ConnectorRequest<?> request, List<ConnectorRequestInterceptor> interceptorChain, HttpClient client) {
+  public HttpRequestInvocation(BasicClassicHttpRequest target, ConnectorRequest<?> request, List<ConnectorRequestInterceptor> interceptorChain, HttpClient client) {
     super(target, request, interceptorChain);
     this.client = client;
   }

connect/soap-http-client/src/test/java/org/camunda/connect/httpclient/soap/SoapHttpConnectorSystemPropertiesTest.java
@@ -14,31 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.connect.soap.httpclient;
+package org.camunda.connect.httpclient.soap;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.http.protocol.HTTP;
-import org.camunda.connect.httpclient.HttpConnector;
-import org.camunda.connect.httpclient.impl.HttpConnectorImpl;
-import org.camunda.connect.httpclient.soap.SoapHttpConnector;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.apache.hc.core5.http.HttpHeaders;
 import org.camunda.connect.httpclient.soap.impl.SoapHttpConnectorImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * Since Apache HTTP client makes it extremely hard to test the proper configuration
@@ -59,7 +54,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
 
   @Before
   public void setUp() {
-    updatedSystemProperties = new HashSet<String>();
+    updatedSystemProperties = new HashSet<>();
     wireMockRule.stubFor(get(urlEqualTo("/")).willReturn(aResponse().withStatus(200)));
   }
 
@@ -74,8 +69,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
     if (!System.getProperties().containsKey(property)) {
       updatedSystemProperties.add(property);
       System.setProperty(property, value);
-    }
-    else {
+    } else {
       throw new RuntimeException("Cannot perform test: System property "
           + property + " is already set. Will not attempt to overwrite this property.");
     }
@@ -92,7 +86,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
     customConnector.createRequest().url("http://localhost:" + PORT).payload("test").execute();
 
     // then
-    verify(postRequestedFor(urlEqualTo("/")).withHeader(HTTP.USER_AGENT, equalTo("foo")));
+    verify(postRequestedFor(urlEqualTo("/")).withHeader(HttpHeaders.USER_AGENT, equalTo("foo")));
 
   }
 }

cibseven/cibseven

connect/http-client/src/main/java/org/cibseven/connect/httpclient/impl/HttpRequestInvocation.java
@@ -18,17 +18,17 @@ package org.cibseven.connect.httpclient.impl;
 
 import java.util.List;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.cibseven.connect.spi.ConnectorRequest;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.core5.http.message.BasicClassicHttpRequest;
 import org.cibseven.connect.impl.AbstractRequestInvocation;
+import org.cibseven.connect.spi.ConnectorRequest;
 import org.cibseven.connect.spi.ConnectorRequestInterceptor;
 
-public class HttpRequestInvocation  extends AbstractRequestInvocation<HttpRequestBase> {
+public class HttpRequestInvocation extends AbstractRequestInvocation<BasicClassicHttpRequest> {
 
   protected HttpClient client;
 
-  public HttpRequestInvocation(HttpRequestBase target, ConnectorRequest<?> request, List<ConnectorRequestInterceptor> interceptorChain, HttpClient client) {
+  public HttpRequestInvocation(BasicClassicHttpRequest target, ConnectorRequest<?> request, List<ConnectorRequestInterceptor> interceptorChain, HttpClient client) {
     super(target, request, interceptorChain);
     this.client = client;
   }

connect/soap-http-client/src/test/java/org/cibseven/connect/httpclient/soap/SoapHttpConnectorSystemPropertiesTest.java
@@ -14,31 +14,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cibseven.connect.soap.httpclient;
+package org.cibseven.connect.httpclient.soap;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.http.protocol.HTTP;
-import org.cibseven.connect.httpclient.HttpConnector;
-import org.cibseven.connect.httpclient.impl.HttpConnectorImpl;
-import org.cibseven.connect.httpclient.soap.SoapHttpConnector;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.apache.hc.core5.http.HttpHeaders;
 import org.cibseven.connect.httpclient.soap.impl.SoapHttpConnectorImpl;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * Since Apache HTTP client makes it extremely hard to test the proper configuration
@@ -59,7 +55,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
 
   @Before
   public void setUp() {
-    updatedSystemProperties = new HashSet<String>();
+    updatedSystemProperties = new HashSet<>();
     wireMockRule.stubFor(get(urlEqualTo("/")).willReturn(aResponse().withStatus(200)));
   }
 
@@ -74,8 +70,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
     if (!System.getProperties().containsKey(property)) {
       updatedSystemProperties.add(property);
       System.setProperty(property, value);
-    }
-    else {
+    } else {
       throw new RuntimeException("Cannot perform test: System property "
           + property + " is already set. Will not attempt to overwrite this property.");
     }
@@ -92,7 +87,7 @@ public class SoapHttpConnectorSystemPropertiesTest {
     customConnector.createRequest().url("http://localhost:" + PORT).payload("test").execute();
 
     // then
-    verify(postRequestedFor(urlEqualTo("/")).withHeader(HTTP.USER_AGENT, equalTo("foo")));
+    verify(postRequestedFor(urlEqualTo("/")).withHeader(HttpHeaders.USER_AGENT, equalTo("foo")));
 
   }
 }