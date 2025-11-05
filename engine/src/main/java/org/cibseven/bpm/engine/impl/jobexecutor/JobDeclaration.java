camunda/camunda-bpm-platform

fix(engine): fix initial retry counter (#5151)
related to: camunda/camunda-bpm-platform/issues/2300

engine/src/main/java/org/camunda/bpm/engine/impl/jobexecutor/JobDeclaration.java
@@ -23,8 +23,11 @@ import java.util.Date;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.impl.batch.BatchEntity;
 import org.camunda.bpm.engine.impl.batch.BatchJobContext;
+import org.camunda.bpm.engine.impl.bpmn.parser.FailedJobRetryConfiguration;
+import org.camunda.bpm.engine.impl.cmd.DefaultJobRetryCmd;
 import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.core.variable.mapping.value.ParameterValueProvider;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
 import org.camunda.bpm.engine.impl.persistence.entity.JobDefinitionEntity;
 import org.camunda.bpm.engine.impl.persistence.entity.JobEntity;
@@ -110,7 +113,32 @@ public abstract class JobDeclaration<S, T extends JobEntity> implements Serializ
     job.setJobHandlerConfiguration(resolveJobHandlerConfiguration(context));
     job.setJobHandlerType(resolveJobHandlerType(context));
     job.setExclusive(resolveExclusive(context));
-    job.setRetries(resolveRetries(context));
+
+    Integer retries = null;
+    CommandContext commandContext = Context.getCommandContext();
+
+    // use legacy behavior if it is enabled
+    // the legacy behavior sets the retries left value to 3 in the context regardless data in the database
+    if (Context.getProcessEngineConfiguration().isLegacyJobRetryBehaviorEnabled()) {
+      job.setRetries(resolveRetries(context));
+    } else {
+      DefaultJobRetryCmd defaultJobRetryCmd = new DefaultJobRetryCmd(job.getId(), null);
+      ActivityImpl currentActivity = defaultJobRetryCmd.getCurrentActivity(commandContext, job);
+      if (currentActivity != null) {
+        FailedJobRetryConfiguration retryConfiguration =
+            defaultJobRetryCmd.getFailedJobRetryConfiguration(job, currentActivity);
+        if (retryConfiguration != null) {
+          retries = retryConfiguration.getRetries();
+        }
+      }
+      // When expression has 0 retries, set initial retries to 1 to avoid immediately raising an incident
+      if (retries != null && retries == 0) {
+        retries = 1;
+      }
+
+      job.setRetries(retries == null ? resolveRetries(context) : retries);
+    }
+
     job.setDuedate(resolveDueDate(context));
 
 

cibseven/cibseven

engine/src/main/java/org/cibseven/bpm/engine/impl/jobexecutor/JobDeclaration.java
@@ -23,8 +23,11 @@ import java.util.Date;
 import org.cibseven.bpm.engine.ProcessEngineConfiguration;
 import org.cibseven.bpm.engine.impl.batch.BatchEntity;
 import org.cibseven.bpm.engine.impl.batch.BatchJobContext;
+import org.cibseven.bpm.engine.impl.bpmn.parser.FailedJobRetryConfiguration;
+import org.cibseven.bpm.engine.impl.cmd.DefaultJobRetryCmd;
 import org.cibseven.bpm.engine.impl.context.Context;
 import org.cibseven.bpm.engine.impl.core.variable.mapping.value.ParameterValueProvider;
+import org.cibseven.bpm.engine.impl.interceptor.CommandContext;
 import org.cibseven.bpm.engine.impl.persistence.entity.ExecutionEntity;
 import org.cibseven.bpm.engine.impl.persistence.entity.JobDefinitionEntity;
 import org.cibseven.bpm.engine.impl.persistence.entity.JobEntity;
@@ -110,7 +113,32 @@ public abstract class JobDeclaration<S, T extends JobEntity> implements Serializ
     job.setJobHandlerConfiguration(resolveJobHandlerConfiguration(context));
     job.setJobHandlerType(resolveJobHandlerType(context));
     job.setExclusive(resolveExclusive(context));
-    job.setRetries(resolveRetries(context));
+
+    Integer retries = null;
+    CommandContext commandContext = Context.getCommandContext();
+
+    // use legacy behavior if it is enabled
+    // the legacy behavior sets the retries left value to 3 in the context regardless data in the database
+    if (Context.getProcessEngineConfiguration().isLegacyJobRetryBehaviorEnabled()) {
+      job.setRetries(resolveRetries(context));
+    } else {
+      DefaultJobRetryCmd defaultJobRetryCmd = new DefaultJobRetryCmd(job.getId(), null);
+      ActivityImpl currentActivity = defaultJobRetryCmd.getCurrentActivity(commandContext, job);
+      if (currentActivity != null) {
+        FailedJobRetryConfiguration retryConfiguration =
+            defaultJobRetryCmd.getFailedJobRetryConfiguration(job, currentActivity);
+        if (retryConfiguration != null) {
+          retries = retryConfiguration.getRetries();
+        }
+      }
+      // When expression has 0 retries, set initial retries to 1 to avoid immediately raising an incident
+      if (retries != null && retries == 0) {
+        retries = 1;
+      }
+
+      job.setRetries(retries == null ? resolveRetries(context) : retries);
+    }
+
     job.setDuedate(resolveDueDate(context));
 
 