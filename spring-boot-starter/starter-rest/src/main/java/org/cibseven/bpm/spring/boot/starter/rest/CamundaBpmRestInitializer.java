camunda/camunda-bpm-platform

chore(spring-boot/rest): expose `FetchAndLockHandler` configurations (#5146)
Exposes the following configurations:
* `camunda.bpm.rest-api.fetch-and-lock.queue-capacity`
* `camunda.bpm.rest-api.fetch-and-lock.unique-worker-request`

related to camunda/camunda-bpm-platform#5140

spring-boot-starter/starter-rest/src/main/java/org/camunda/bpm/spring/boot/starter/rest/CamundaBpmRestInitializer.java
@@ -18,6 +18,7 @@ package org.camunda.bpm.spring.boot.starter.rest;
 
 import org.camunda.bpm.engine.rest.filter.CacheControlFilter;
 import org.camunda.bpm.engine.rest.filter.EmptyBodyFilter;
+import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
@@ -27,7 +28,7 @@ import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterRegistration;
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
+
 import java.util.EnumSet;
 import java.util.Map;
 
@@ -46,13 +47,18 @@ public class CamundaBpmRestInitializer implements ServletContextInitializer {
 
   private JerseyApplicationPath applicationPath;
 
-  public CamundaBpmRestInitializer(JerseyApplicationPath applicationPath) {
+  private final CamundaBpmProperties properties;
+
+  public CamundaBpmRestInitializer(JerseyApplicationPath applicationPath, CamundaBpmProperties properties) {
     this.applicationPath = applicationPath;
+    this.properties = properties;
   }
 
   @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
+  public void onStartup(ServletContext servletContext) {
     this.servletContext = servletContext;
+
+    properties.getRestApi().getFetchAndLock().getInitParams().forEach(servletContext::setInitParameter);
 
     String restApiPathPattern = applicationPath.getUrlMapping();
 
@@ -81,4 +87,5 @@ public class CamundaBpmRestInitializer implements ServletContextInitializer {
 
     return filterRegistration;
   }
+
 }

spring-boot-starter/starter-rest/src/main/java/org/camunda/bpm/spring/boot/starter/rest/CamundaBpmRestJerseyAutoConfiguration.java
@@ -18,6 +18,7 @@ package org.camunda.bpm.spring.boot.starter.rest;
 
 import org.camunda.bpm.engine.rest.impl.FetchAndLockContextListener;
 import org.camunda.bpm.spring.boot.starter.CamundaBpmAutoConfiguration;
+import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -41,7 +42,7 @@ public class CamundaBpmRestJerseyAutoConfiguration {
   }
 
   @Bean
-  public CamundaBpmRestInitializer camundaBpmRestInitializer(JerseyApplicationPath applicationPath) {
-    return new CamundaBpmRestInitializer(applicationPath);
+  public CamundaBpmRestInitializer camundaBpmRestInitializer(JerseyApplicationPath applicationPath, CamundaBpmProperties props) {
+    return new CamundaBpmRestInitializer(applicationPath, props);
   }
 }

cibseven/cibseven

spring-boot-starter/starter-rest/src/main/java/org/cibseven/bpm/spring/boot/starter/rest/CamundaBpmRestInitializer.java
@@ -21,6 +21,7 @@ import java.util.Map;
 
 import org.cibseven.bpm.engine.rest.filter.CacheControlFilter;
 import org.cibseven.bpm.engine.rest.filter.EmptyBodyFilter;
+import org.cibseven.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
@@ -30,7 +31,7 @@ import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterRegistration;
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
+
 
 /**
  * Inspired by:
@@ -47,13 +48,18 @@ public class CamundaBpmRestInitializer implements ServletContextInitializer {
 
   private JerseyApplicationPath applicationPath;
 
-  public CamundaBpmRestInitializer(JerseyApplicationPath applicationPath) {
+  private final CamundaBpmProperties properties;
+
+  public CamundaBpmRestInitializer(JerseyApplicationPath applicationPath, CamundaBpmProperties properties) {
     this.applicationPath = applicationPath;
+    this.properties = properties;
   }
 
   @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
+  public void onStartup(ServletContext servletContext) {
     this.servletContext = servletContext;
+
+    properties.getRestApi().getFetchAndLock().getInitParams().forEach(servletContext::setInitParameter);
 
     String restApiPathPattern = applicationPath.getUrlMapping();
 
@@ -82,4 +88,5 @@ public class CamundaBpmRestInitializer implements ServletContextInitializer {
 
     return filterRegistration;
   }
+
 }

spring-boot-starter/starter-rest/src/main/java/org/cibseven/bpm/spring/boot/starter/rest/CamundaBpmRestJerseyAutoConfiguration.java
@@ -18,6 +18,7 @@ package org.cibseven.bpm.spring.boot.starter.rest;
 
 import org.cibseven.bpm.engine.rest.impl.FetchAndLockContextListener;
 import org.cibseven.bpm.spring.boot.starter.CamundaBpmAutoConfiguration;
+import org.cibseven.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -41,7 +42,7 @@ public class CamundaBpmRestJerseyAutoConfiguration {
   }
 
   @Bean
-  public CamundaBpmRestInitializer camundaBpmRestInitializer(JerseyApplicationPath applicationPath) {
-    return new CamundaBpmRestInitializer(applicationPath);
+  public CamundaBpmRestInitializer camundaBpmRestInitializer(JerseyApplicationPath applicationPath, CamundaBpmProperties props) {
+    return new CamundaBpmRestInitializer(applicationPath, props);
   }
 }