camunda/camunda-bpm-platform

chore(distro): add support for WildFly 37 (#5263)
related to camunda/camunda-bpm-platform#5045

distro/wildfly/subsystem/src/main/java/org/camunda/bpm/container/impl/jboss/extension/handler/EnhancedQueueExecutorService.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.container.impl.jboss.extension.handler;
+
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StopContext;
+import org.jboss.threads.EnhancedQueueExecutor;
+import org.jboss.threads.EnhancedQueueExecutor.Builder;
+
+public class EnhancedQueueExecutorService implements Service<EnhancedQueueExecutor> {
+
+  protected Builder executorBuilder;
+  protected EnhancedQueueExecutor executor;
+
+  public EnhancedQueueExecutorService(Builder executorBuilder) {
+    this.executorBuilder = executorBuilder;
+  }
+
+  @Override
+  public void start(StartContext context) {
+    executor = executorBuilder.build();
+  }
+
+  @Override
+  public void stop(StopContext context) {
+    if (executor != null) {
+      executor.shutdown();
+    }
+  }
+
+  @Override
+  public EnhancedQueueExecutor getValue() throws IllegalStateException, IllegalArgumentException {
+    return executor;
+  }
+}

distro/wildfly/subsystem/src/main/java/org/camunda/bpm/container/impl/jboss/extension/handler/JobExecutorAdd.java
@@ -16,29 +16,33 @@
  */
 package org.camunda.bpm.container.impl.jboss.extension.handler;
 
-import java.util.concurrent.ThreadFactory;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.ALLOW_CORE_TIMEOUT;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.CORE_THREADS;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.JOB_EXECUTOR_ATTRIBUTES;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.KEEPALIVE_TIME;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.MAX_THREADS;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.QUEUE_LENGTH;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.THREAD_POOL_NAME;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.camunda.bpm.container.ExecutorService;
-import org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons;
 import org.camunda.bpm.container.impl.jboss.service.MscExecutorService;
 import org.camunda.bpm.container.impl.jboss.service.ServiceNames;
 import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.threads.BoundedQueueThreadPoolService;
-import org.jboss.as.threads.ManagedQueueExecutorService;
 import org.jboss.as.threads.ThreadFactoryService;
-import org.jboss.as.threads.TimeSpec;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
-import org.jboss.msc.service.ServiceController.Mode;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.ServiceTarget;
-
+import org.jboss.threads.EnhancedQueueExecutor;
+import org.jboss.threads.EnhancedQueueExecutor.Builder;
 
 /**
  * Installs the JobExecutor service into the container.
@@ -52,15 +56,13 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
   public static final JobExecutorAdd INSTANCE = new JobExecutorAdd();
 
   protected JobExecutorAdd() {
-    super(SubsystemAttributeDefinitons.JOB_EXECUTOR_ATTRIBUTES);
+    super(JOB_EXECUTOR_ATTRIBUTES);
   }
 
   @Override
-  protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model)
-      throws OperationFailedException {
+  protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model) throws OperationFailedException {
 
-    String jobExecutorThreadPoolName = SubsystemAttributeDefinitons.THREAD_POOL_NAME
-        .resolveModelAttribute(context, model).asString();
+    String jobExecutorThreadPoolName = THREAD_POOL_NAME.resolveModelAttribute(context, model).asString();
     ServiceName jobExecutorThreadPoolServiceName = ServiceNames.forManagedThreadPool(jobExecutorThreadPoolName);
 
     performRuntimeThreadPool(context, model, jobExecutorThreadPoolName, jobExecutorThreadPoolServiceName);
@@ -68,15 +70,18 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
     ServiceName serviceName = ServiceNames.forMscExecutorService();
     ServiceBuilder<?> builder = context.getServiceTarget().addService(serviceName);
     Consumer<ExecutorService> provider = builder.provides(serviceName);
-    Supplier<ManagedQueueExecutorService> supplier = builder.requires(jobExecutorThreadPoolServiceName);
+    Supplier<EnhancedQueueExecutor> supplier = builder.requires(jobExecutorThreadPoolServiceName);
     MscExecutorService service = new MscExecutorService(supplier, provider);
-    builder.setInitialMode(Mode.ACTIVE);
+    builder.setInitialMode(ServiceController.Mode.ACTIVE);
     builder.setInstance(service);
     builder.install();
   }
 
-  protected void performRuntimeThreadPool(OperationContext context, ModelNode model, String name,
-      ServiceName jobExecutorThreadPoolServiceName) throws OperationFailedException {
+  protected void performRuntimeThreadPool(OperationContext context,
+                                          ModelNode model,
+                                          String name,
+                                          ServiceName jobExecutorThreadPoolServiceName)
+      throws OperationFailedException {
 
     ServiceTarget serviceTarget = context.getServiceTarget();
 
@@ -87,18 +92,16 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
 
     serviceTarget.addService(threadFactoryServiceName, threadFactory).install();
 
-    final BoundedQueueThreadPoolService threadPoolService = new BoundedQueueThreadPoolService(
-        SubsystemAttributeDefinitons.CORE_THREADS.resolveModelAttribute(context, model).asInt(),
-        SubsystemAttributeDefinitons.MAX_THREADS.resolveModelAttribute(context, model).asInt(),
-        SubsystemAttributeDefinitons.QUEUE_LENGTH.resolveModelAttribute(context, model).asInt(), false,
-        new TimeSpec(TimeUnit.SECONDS,
-                SubsystemAttributeDefinitons.KEEPALIVE_TIME.resolveModelAttribute(context, model).asInt()),
-        SubsystemAttributeDefinitons.ALLOW_CORE_TIMEOUT.resolveModelAttribute(context, model).asBoolean());
+    Builder executorBuilder = new Builder()
+        .setMaximumPoolSize(MAX_THREADS.resolveModelAttribute(context, model).asInt())
+        .setCorePoolSize(CORE_THREADS.resolveModelAttribute(context, model).asInt())
+        .setMaximumQueueSize(QUEUE_LENGTH.resolveModelAttribute(context, model).asInt())
+        .setKeepAliveTime(KEEPALIVE_TIME.resolveModelAttribute(context, model).asInt(), TimeUnit.SECONDS)
+        .allowCoreThreadTimeOut(ALLOW_CORE_TIMEOUT.resolveModelAttribute(context, model).asBoolean());
 
-    serviceTarget.addService(jobExecutorThreadPoolServiceName, threadPoolService)
-        .addDependency(threadFactoryServiceName, ThreadFactory.class,
-                threadPoolService.getThreadFactoryInjector())
-        .setInitialMode(ServiceController.Mode.ACTIVE).install();
+    ServiceBuilder<EnhancedQueueExecutor> queueExecutorBuilder = serviceTarget.addService(jobExecutorThreadPoolServiceName, new EnhancedQueueExecutorService(executorBuilder));
+    queueExecutorBuilder.requires(threadFactoryServiceName);
+    queueExecutorBuilder.setInitialMode(ServiceController.Mode.ACTIVE).install();
   }
 
 }

distro/wildfly/subsystem/src/test/java/org/camunda/bpm/container/impl/jboss/test/JBossSubsystemXMLTest.java
@@ -16,6 +16,10 @@
  */
 package org.camunda.bpm.container.impl.jboss.test;
 
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_CORE_THREADS;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_KEEPALIVE_TIME;
+import static org.camunda.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_MAX_THREADS;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
@@ -27,16 +31,14 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-
 import javax.xml.stream.XMLStreamException;
-
 import org.camunda.bpm.container.impl.jboss.config.ManagedProcessEngineMetadata;
 import org.camunda.bpm.container.impl.jboss.extension.Attribute;
 import org.camunda.bpm.container.impl.jboss.extension.BpmPlatformExtension;
@@ -55,14 +57,13 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.naming.deployment.ContextNames;
 import org.jboss.as.subsystem.test.AbstractSubsystemTest;
 import org.jboss.as.subsystem.test.KernelServices;
-import org.jboss.as.threads.ManagedQueueExecutorService;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.ModelType;
 import org.jboss.msc.service.ServiceContainer;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
+import org.jboss.threads.EnhancedQueueExecutor;
 import org.junit.Test;
-
 
 /**
  *
@@ -469,19 +470,17 @@ public class JBossSubsystemXMLTest extends AbstractSubsystemTest {
     assertEquals(3, defaultJobExecutor.getMaxJobsPerAcquisition());
 
     // ServiceName: 'org.camunda.bpm.platform.job-executor.job-executor-tp'
-    ServiceController<?> managedQueueExecutorServiceController = container.getService(ServiceNames.forManagedThreadPool(SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
-    assertNotNull(managedQueueExecutorServiceController);
-    Object managedQueueExecutorServiceObject = managedQueueExecutorServiceController.getValue();
-    assertNotNull(managedQueueExecutorServiceObject);
-    assertTrue(managedQueueExecutorServiceObject instanceof ManagedQueueExecutorService);
-    ManagedQueueExecutorService managedQueueExecutorService = (ManagedQueueExecutorService) managedQueueExecutorServiceObject;
-    assertEquals("Number of core threads is wrong", SubsystemAttributeDefinitons.DEFAULT_CORE_THREADS, managedQueueExecutorService.getCoreThreads());
-    assertEquals("Number of max threads is wrong", SubsystemAttributeDefinitons.DEFAULT_MAX_THREADS, managedQueueExecutorService.getMaxThreads());
-    assertEquals(SubsystemAttributeDefinitons.DEFAULT_KEEPALIVE_TIME, TimeUnit.NANOSECONDS.toSeconds(managedQueueExecutorService.getKeepAlive()));
-    assertEquals(false, managedQueueExecutorService.isBlocking());
-    assertEquals(SubsystemAttributeDefinitons.DEFAULT_ALLOW_CORE_TIMEOUT, managedQueueExecutorService.isAllowCoreTimeout());
+    ServiceController<?> EnhancedQueueExecutorController = container.getService(ServiceNames.forManagedThreadPool(DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
+    assertNotNull(EnhancedQueueExecutorController);
+    Object EnhancedQueueExecutorObject = EnhancedQueueExecutorController.getValue();
+    assertNotNull(EnhancedQueueExecutorObject);
+    assertTrue(EnhancedQueueExecutorObject instanceof EnhancedQueueExecutor);
+    EnhancedQueueExecutor enhancedQueueExecutor = (EnhancedQueueExecutor) EnhancedQueueExecutorObject;
+    assertEquals("Number of core threads is wrong", DEFAULT_CORE_THREADS, enhancedQueueExecutor.getCorePoolSize());
+    assertEquals("Number of max threads is wrong", DEFAULT_MAX_THREADS, enhancedQueueExecutor.getMaximumPoolSize());
+    assertEquals(DEFAULT_KEEPALIVE_TIME, enhancedQueueExecutor.getKeepAliveTime().toSeconds());
 
-    ServiceController<?> threadFactoryService = container.getService(ServiceNames.forThreadFactoryService(SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
+    ServiceController<?> threadFactoryService = container.getService(ServiceNames.forThreadFactoryService(DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
     assertNotNull(threadFactoryService);
     assertTrue(threadFactoryService.getValue() instanceof ThreadFactory);
 

cibseven/cibseven

distro/wildfly/subsystem/src/main/java/org/cibseven/bpm/container/impl/jboss/extension/handler/EnhancedQueueExecutorService.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cibseven.bpm.container.impl.jboss.extension.handler;
+
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StopContext;
+import org.jboss.threads.EnhancedQueueExecutor;
+import org.jboss.threads.EnhancedQueueExecutor.Builder;
+
+public class EnhancedQueueExecutorService implements Service<EnhancedQueueExecutor> {
+
+  protected Builder executorBuilder;
+  protected EnhancedQueueExecutor executor;
+
+  public EnhancedQueueExecutorService(Builder executorBuilder) {
+    this.executorBuilder = executorBuilder;
+  }
+
+  @Override
+  public void start(StartContext context) {
+    executor = executorBuilder.build();
+  }
+
+  @Override
+  public void stop(StopContext context) {
+    if (executor != null) {
+      executor.shutdown();
+    }
+  }
+
+  @Override
+  public EnhancedQueueExecutor getValue() throws IllegalStateException, IllegalArgumentException {
+    return executor;
+  }
+}

distro/wildfly/subsystem/src/main/java/org/cibseven/bpm/container/impl/jboss/extension/handler/JobExecutorAdd.java
@@ -16,29 +16,32 @@
  */
 package org.cibseven.bpm.container.impl.jboss.extension.handler;
 
-import java.util.concurrent.ThreadFactory;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.ALLOW_CORE_TIMEOUT;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.CORE_THREADS;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.JOB_EXECUTOR_ATTRIBUTES;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.KEEPALIVE_TIME;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.MAX_THREADS;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.QUEUE_LENGTH;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.THREAD_POOL_NAME;
+
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.cibseven.bpm.container.ExecutorService;
-import org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons;
 import org.cibseven.bpm.container.impl.jboss.service.MscExecutorService;
 import org.cibseven.bpm.container.impl.jboss.service.ServiceNames;
 import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.threads.BoundedQueueThreadPoolService;
-import org.jboss.as.threads.ManagedQueueExecutorService;
 import org.jboss.as.threads.ThreadFactoryService;
-import org.jboss.as.threads.TimeSpec;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
-import org.jboss.msc.service.ServiceController.Mode;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.ServiceTarget;
-
+import org.jboss.threads.EnhancedQueueExecutor;
+import org.jboss.threads.EnhancedQueueExecutor.Builder;
 
 /**
  * Installs the JobExecutor service into the container.
@@ -52,15 +55,13 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
   public static final JobExecutorAdd INSTANCE = new JobExecutorAdd();
 
   protected JobExecutorAdd() {
-    super(SubsystemAttributeDefinitons.JOB_EXECUTOR_ATTRIBUTES);
+    super(JOB_EXECUTOR_ATTRIBUTES);
   }
 
   @Override
-  protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model)
-      throws OperationFailedException {
+  protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model) throws OperationFailedException {
 
-    String jobExecutorThreadPoolName = SubsystemAttributeDefinitons.THREAD_POOL_NAME
-        .resolveModelAttribute(context, model).asString();
+    String jobExecutorThreadPoolName = THREAD_POOL_NAME.resolveModelAttribute(context, model).asString();
     ServiceName jobExecutorThreadPoolServiceName = ServiceNames.forManagedThreadPool(jobExecutorThreadPoolName);
 
     performRuntimeThreadPool(context, model, jobExecutorThreadPoolName, jobExecutorThreadPoolServiceName);
@@ -68,15 +69,18 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
     ServiceName serviceName = ServiceNames.forMscExecutorService();
     ServiceBuilder<?> builder = context.getServiceTarget().addService(serviceName);
     Consumer<ExecutorService> provider = builder.provides(serviceName);
-    Supplier<ManagedQueueExecutorService> supplier = builder.requires(jobExecutorThreadPoolServiceName);
+    Supplier<EnhancedQueueExecutor> supplier = builder.requires(jobExecutorThreadPoolServiceName);
     MscExecutorService service = new MscExecutorService(supplier, provider);
-    builder.setInitialMode(Mode.ACTIVE);
+    builder.setInitialMode(ServiceController.Mode.ACTIVE);
     builder.setInstance(service);
     builder.install();
   }
 
-  protected void performRuntimeThreadPool(OperationContext context, ModelNode model, String name,
-      ServiceName jobExecutorThreadPoolServiceName) throws OperationFailedException {
+  protected void performRuntimeThreadPool(OperationContext context,
+                                          ModelNode model,
+                                          String name,
+                                          ServiceName jobExecutorThreadPoolServiceName)
+      throws OperationFailedException {
 
     ServiceTarget serviceTarget = context.getServiceTarget();
 
@@ -87,18 +91,16 @@ public class JobExecutorAdd extends AbstractAddStepHandler {
 
     serviceTarget.addService(threadFactoryServiceName, threadFactory).install();
 
-    final BoundedQueueThreadPoolService threadPoolService = new BoundedQueueThreadPoolService(
-        SubsystemAttributeDefinitons.CORE_THREADS.resolveModelAttribute(context, model).asInt(),
-        SubsystemAttributeDefinitons.MAX_THREADS.resolveModelAttribute(context, model).asInt(),
-        SubsystemAttributeDefinitons.QUEUE_LENGTH.resolveModelAttribute(context, model).asInt(), false,
-        new TimeSpec(TimeUnit.SECONDS,
-                SubsystemAttributeDefinitons.KEEPALIVE_TIME.resolveModelAttribute(context, model).asInt()),
-        SubsystemAttributeDefinitons.ALLOW_CORE_TIMEOUT.resolveModelAttribute(context, model).asBoolean());
+    Builder executorBuilder = new Builder()
+        .setMaximumPoolSize(MAX_THREADS.resolveModelAttribute(context, model).asInt())
+        .setCorePoolSize(CORE_THREADS.resolveModelAttribute(context, model).asInt())
+        .setMaximumQueueSize(QUEUE_LENGTH.resolveModelAttribute(context, model).asInt())
+        .setKeepAliveTime(KEEPALIVE_TIME.resolveModelAttribute(context, model).asInt(), TimeUnit.SECONDS)
+        .allowCoreThreadTimeOut(ALLOW_CORE_TIMEOUT.resolveModelAttribute(context, model).asBoolean());
 
-    serviceTarget.addService(jobExecutorThreadPoolServiceName, threadPoolService)
-        .addDependency(threadFactoryServiceName, ThreadFactory.class,
-                threadPoolService.getThreadFactoryInjector())
-        .setInitialMode(ServiceController.Mode.ACTIVE).install();
+    ServiceBuilder<EnhancedQueueExecutor> queueExecutorBuilder = serviceTarget.addService(jobExecutorThreadPoolServiceName, new EnhancedQueueExecutorService(executorBuilder));
+    queueExecutorBuilder.requires(threadFactoryServiceName);
+    queueExecutorBuilder.setInitialMode(ServiceController.Mode.ACTIVE).install();
   }
 
 }

distro/wildfly26/subsystem/src/test/java/org/cibseven/bpm/container/impl/jboss/test/JBossSubsystemXMLTest.java
@@ -16,6 +16,10 @@
  */
 package org.cibseven.bpm.container.impl.jboss.test;
 
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_CORE_THREADS;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_KEEPALIVE_TIME;
+import static org.cibseven.bpm.container.impl.jboss.extension.SubsystemAttributeDefinitons.DEFAULT_MAX_THREADS;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
@@ -27,16 +31,14 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-
 import javax.xml.stream.XMLStreamException;
-
 import org.cibseven.bpm.container.impl.jboss.config.ManagedProcessEngineMetadata;
 import org.cibseven.bpm.container.impl.jboss.extension.Attribute;
 import org.cibseven.bpm.container.impl.jboss.extension.BpmPlatformExtension;
@@ -55,14 +57,13 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.naming.deployment.ContextNames;
 import org.jboss.as.subsystem.test.AbstractSubsystemTest;
 import org.jboss.as.subsystem.test.KernelServices;
-import org.jboss.as.threads.ManagedQueueExecutorService;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.ModelType;
 import org.jboss.msc.service.ServiceContainer;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
+import org.jboss.threads.EnhancedQueueExecutor;
 import org.junit.Test;
-
 
 /**
  *
@@ -469,19 +470,17 @@ public class JBossSubsystemXMLTest extends AbstractSubsystemTest {
     assertEquals(3, defaultJobExecutor.getMaxJobsPerAcquisition());
 
     // ServiceName: 'org.cibseven.bpm.platform.job-executor.job-executor-tp'
-    ServiceController<?> managedQueueExecutorServiceController = container.getService(ServiceNames.forManagedThreadPool(SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
-    assertNotNull(managedQueueExecutorServiceController);
-    Object managedQueueExecutorServiceObject = managedQueueExecutorServiceController.getValue();
-    assertNotNull(managedQueueExecutorServiceObject);
-    assertTrue(managedQueueExecutorServiceObject instanceof ManagedQueueExecutorService);
-    ManagedQueueExecutorService managedQueueExecutorService = (ManagedQueueExecutorService) managedQueueExecutorServiceObject;
-    assertEquals("Number of core threads is wrong", SubsystemAttributeDefinitons.DEFAULT_CORE_THREADS, managedQueueExecutorService.getCoreThreads());
-    assertEquals("Number of max threads is wrong", SubsystemAttributeDefinitons.DEFAULT_MAX_THREADS, managedQueueExecutorService.getMaxThreads());
-    assertEquals(SubsystemAttributeDefinitons.DEFAULT_KEEPALIVE_TIME, TimeUnit.NANOSECONDS.toSeconds(managedQueueExecutorService.getKeepAlive()));
-    assertEquals(false, managedQueueExecutorService.isBlocking());
-    assertEquals(SubsystemAttributeDefinitons.DEFAULT_ALLOW_CORE_TIMEOUT, managedQueueExecutorService.isAllowCoreTimeout());
+    ServiceController<?> EnhancedQueueExecutorController = container.getService(ServiceNames.forManagedThreadPool(DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
+    assertNotNull(EnhancedQueueExecutorController);
+    Object EnhancedQueueExecutorObject = EnhancedQueueExecutorController.getValue();
+    assertNotNull(EnhancedQueueExecutorObject);
+    assertTrue(EnhancedQueueExecutorObject instanceof EnhancedQueueExecutor);
+    EnhancedQueueExecutor enhancedQueueExecutor = (EnhancedQueueExecutor) EnhancedQueueExecutorObject;
+    assertEquals("Number of core threads is wrong", DEFAULT_CORE_THREADS, enhancedQueueExecutor.getCorePoolSize());
+    assertEquals("Number of max threads is wrong", DEFAULT_MAX_THREADS, enhancedQueueExecutor.getMaximumPoolSize());
+    assertEquals(DEFAULT_KEEPALIVE_TIME, enhancedQueueExecutor.getKeepAliveTime().toSeconds());
 
-    ServiceController<?> threadFactoryService = container.getService(ServiceNames.forThreadFactoryService(SubsystemAttributeDefinitons.DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
+    ServiceController<?> threadFactoryService = container.getService(ServiceNames.forThreadFactoryService(DEFAULT_JOB_EXECUTOR_THREADPOOL_NAME));
     assertNotNull(threadFactoryService);
     assertTrue(threadFactoryService.getValue() instanceof ThreadFactory);
 