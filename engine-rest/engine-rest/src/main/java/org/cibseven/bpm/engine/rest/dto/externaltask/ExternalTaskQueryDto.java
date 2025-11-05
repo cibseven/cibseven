camunda/camunda-bpm-platform

feat(engine): variable and process definition queries for external tasks (#5335)
* feat(engine): variable and process definition queries for external tasks

- adds functionality to filter external tasks based on process variables
- adds functionality to filter external tasks based on process definition key
- adds functionality to filter external tasks based on process definition name

related to https://github.com/camunda/camunda-bpm-platform/issues/5334

* feat(rest): variable and process definition queries for external tasks

- adds functionality to the rest external task query dto based on process variables
- adds functionality to the rest external task query dto based on process definition key
- adds functionality to the rest external task query dto based on process definition name

related to https://github.com/camunda/camunda-bpm-platform/issues/5334

* feat(rest): variable and process definition queries for external tasks

- fixes ExternalTaskRestServiceQueryTest.testCompleteGETQuery

related to https://github.com/camunda/camunda-bpm-platform/issues/5334

engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/dto/externaltask/ExternalTaskQueryDto.java
@@ -23,15 +23,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.externaltask.ExternalTaskQuery;
 import org.camunda.bpm.engine.rest.dto.AbstractQueryDto;
 import org.camunda.bpm.engine.rest.dto.CamundaQueryParam;
-import org.camunda.bpm.engine.rest.dto.converter.BooleanConverter;
-import org.camunda.bpm.engine.rest.dto.converter.DateConverter;
-import org.camunda.bpm.engine.rest.dto.converter.LongConverter;
-import org.camunda.bpm.engine.rest.dto.converter.StringListConverter;
-import org.camunda.bpm.engine.rest.dto.converter.StringSetConverter;
+import org.camunda.bpm.engine.rest.dto.VariableQueryParameterDto;
+import org.camunda.bpm.engine.rest.dto.converter.*;
+import org.camunda.bpm.engine.rest.exception.InvalidRequestException;
 
 /**
  * @author Thorben Lindhauer
@@ -62,7 +62,11 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   protected String executionId;
   protected String processInstanceId;
   protected List<String> processInstanceIdIn;
+  protected String processDefinitionKey;
+  protected String[] processDefinitionKeyIn;
   protected String processDefinitionId;
+  protected String processDefinitionName;
+  protected String processDefinitionNameLike;
   protected Boolean active;
   protected Boolean suspended;
   protected Boolean withRetriesLeft;
@@ -71,6 +75,10 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   protected List<String> tenantIds;
   protected Long priorityHigherThanOrEquals;
   protected Long priorityLowerThanOrEquals;
+  protected Boolean variableNamesIgnoreCase;
+  protected Boolean variableValuesIgnoreCase;
+
+  private List<VariableQueryParameterDto> processVariables;
 
   public ExternalTaskQueryDto() {
   }
@@ -143,9 +151,29 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     return processDefinitionId;
   }
 
+  @CamundaQueryParam("processDefinitionKey")
+  public void setProcessDefinitionKey(String processDefinitionKey) {
+    this.processDefinitionKey = processDefinitionKey;
+  }
+
+  @CamundaQueryParam(value = "processDefinitionKeyIn", converter = StringArrayConverter.class)
+  public void setProcessDefinitionKeyIn(String[] processDefinitionKeyIn) {
+    this.processDefinitionKeyIn = processDefinitionKeyIn;
+  }
+
   @CamundaQueryParam("processDefinitionId")
   public void setProcessDefinitionId(String processDefinitionId) {
     this.processDefinitionId = processDefinitionId;
+  }
+
+  @CamundaQueryParam("processDefinitionName")
+  public void setProcessDefinitionName(String processDefinitionName) {
+    this.processDefinitionName = processDefinitionName;
+  }
+
+  @CamundaQueryParam("processDefinitionNameLike")
+  public void setProcessDefinitionNameLike(String processDefinitionNameLike) {
+    this.processDefinitionNameLike = processDefinitionNameLike;
   }
 
   @CamundaQueryParam(value = "active", converter = BooleanConverter.class)
@@ -180,6 +208,21 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   @CamundaQueryParam(value="priorityHigherThanOrEquals", converter = LongConverter.class)
   public void setPriorityHigherThanOrEquals(Long priorityHigherThanOrEquals) {
     this.priorityHigherThanOrEquals = priorityHigherThanOrEquals;
+  }
+
+  @CamundaQueryParam(value = "variableNamesIgnoreCase", converter = BooleanConverter.class)
+  public void setVariableNamesIgnoreCase(Boolean variableNamesCaseInsensitive) {
+    this.variableNamesIgnoreCase = variableNamesCaseInsensitive;
+  }
+
+  @CamundaQueryParam(value ="variableValuesIgnoreCase", converter = BooleanConverter.class)
+  public void setVariableValuesIgnoreCase(Boolean variableValuesCaseInsensitive) {
+    this.variableValuesIgnoreCase = variableValuesCaseInsensitive;
+  }
+
+  @CamundaQueryParam(value = "processVariables", converter = VariableListConverter.class)
+  public void setProcessVariables(List<VariableQueryParameterDto> processVariables) {
+    this.processVariables = processVariables;
   }
 
   @CamundaQueryParam(value="priorityLowerThanOrEquals", converter = LongConverter.class)
@@ -235,8 +278,20 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     if (processInstanceIdIn != null && !processInstanceIdIn.isEmpty()) {
       query.processInstanceIdIn(processInstanceIdIn.toArray(new String[0]));
     }
+    if (processDefinitionKey != null) {
+      query.processDefinitionKey(processDefinitionKey);
+    }
+    if (processDefinitionKeyIn != null && processDefinitionKeyIn.length > 0) {
+      query.processDefinitionKeyIn(processDefinitionKeyIn);
+    }
     if (processDefinitionId != null) {
       query.processDefinitionId(processDefinitionId);
+    }
+    if (processDefinitionName != null) {
+      query.processDefinitionName(processDefinitionName);
+    }
+    if (processDefinitionNameLike != null) {
+      query.processDefinitionNameLike(processDefinitionNameLike);
     }
     if (active != null && active) {
       query.active();
@@ -255,6 +310,38 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     }
     if (noRetriesLeft != null && noRetriesLeft) {
       query.noRetriesLeft();
+    }
+    if(variableValuesIgnoreCase != null && variableValuesIgnoreCase) {
+      query.matchVariableValuesIgnoreCase();
+    }
+    if(variableNamesIgnoreCase != null && variableNamesIgnoreCase) {
+      query.matchVariableNamesIgnoreCase();
+    }
+    if (processVariables != null) {
+      for (VariableQueryParameterDto variableQueryParam : processVariables) {
+        String variableName = variableQueryParam.getName();
+        String op = variableQueryParam.getOperator();
+        Object variableValue = variableQueryParam.resolveValue(objectMapper);
+        if (op.equals(VariableQueryParameterDto.EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.NOT_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueNotEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.GREATER_THAN_OPERATOR_NAME)) {
+          query.processVariableValueGreaterThan(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.GREATER_THAN_OR_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueGreaterThanOrEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LESS_THAN_OPERATOR_NAME)) {
+          query.processVariableValueLessThan(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LESS_THAN_OR_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueLessThanOrEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LIKE_OPERATOR_NAME)) {
+          query.processVariableValueLike(variableName, String.valueOf(variableValue));
+        } else if (op.equals(VariableQueryParameterDto.NOT_LIKE_OPERATOR_NAME)) {
+          query.processVariableValueNotLike(variableName, String.valueOf(variableValue));
+        } else {
+          throw new InvalidRequestException(Response.Status.BAD_REQUEST, "Invalid process variable comparator specified: " + op);
+        }
+      }
     }
     if (workerId != null) {
       query.workerId(workerId);

engine/src/main/java/org/camunda/bpm/engine/externaltask/ExternalTaskQuery.java
@@ -16,10 +16,11 @@
  */
 package org.camunda.bpm.engine.externaltask;
 
+import org.camunda.bpm.engine.query.Query;
+
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
-
-import org.camunda.bpm.engine.query.Query;
 
 /**
  * @author Thorben Lindhauer
@@ -85,9 +86,34 @@ public interface ExternalTaskQuery extends Query<ExternalTaskQuery, ExternalTask
   ExternalTaskQuery processInstanceIdIn(String... processInstanceIdIn);
 
   /**
+   * Only select tasks which are part of a process instance which has the given
+   * process definition key.
+   */
+  ExternalTaskQuery processDefinitionKey(String processDefinitionKey);
+
+  /**
+   * Only select tasks which are part of a process instance which has one of the
+   * given process definition keys.
+   */
+  ExternalTaskQuery processDefinitionKeyIn(String... processDefinitionKeys);
+
+  /**
    * Only select external tasks that belong to an instance of the given process definition
    */
   ExternalTaskQuery processDefinitionId(String processDefinitionId);
+
+  /**
+   * Only select tasks which are part of a process instance which has the given
+   * process definition name.
+   */
+  ExternalTaskQuery processDefinitionName(String processDefinitionName);
+
+  /**
+   * Only select tasks which are part of a process instance which process definition
+   * name  is like the given parameter.
+   * The syntax is that of SQL: for example usage: nameLike(%processDefinitionName%)
+   */
+  ExternalTaskQuery processDefinitionNameLike(String processDefinitionName);
 
   /**
    * Only select external tasks that belong to an instance of the given activity
@@ -116,6 +142,68 @@ public interface ExternalTaskQuery extends Query<ExternalTaskQuery, ExternalTask
    * @return the builded external task query
    */
   ExternalTaskQuery priorityLowerThanOrEquals(long priority);
+
+  /**
+   * Only select tasks which have are part of a process that have a variable
+   * with the given name set to the given value.
+   */
+  ExternalTaskQuery processVariableValueEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which have a variable with the given name, but
+   * with a different value than the passed value.
+   * Byte-arrays and {@link Serializable} objects (which are not primitive type wrappers)
+   * are not supported.
+   */
+  ExternalTaskQuery processVariableValueNotEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and matching the given value.
+   * The syntax is that of SQL: for example usage: valueLike(%value%)
+   */
+  ExternalTaskQuery processVariableValueLike(String variableName, String variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and not matching the given value.
+   * The syntax is that of SQL: for example usage: valueNotLike(%value%)
+   */
+  ExternalTaskQuery processVariableValueNotLike(String variableName, String variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than the given one.
+   */
+  ExternalTaskQuery processVariableValueGreaterThan(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than or equal to the given one.
+   */
+  ExternalTaskQuery processVariableValueGreaterThanOrEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value less than the given one.
+   */
+  ExternalTaskQuery processVariableValueLessThan(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than or equal to the given one.
+   */
+  ExternalTaskQuery processVariableValueLessThanOrEquals(String variableName, Object variableValue);
+
+  /**
+   * All queries for task-, process- and case-variables will match the variable names in a case-insensitive way.
+   */
+  ExternalTaskQuery matchVariableNamesIgnoreCase();
+
+  /**
+   * All queries for task-, process- and case-variables will match the variable values in a case-insensitive way.
+   */
+  ExternalTaskQuery matchVariableValuesIgnoreCase();
 
   /**
    * Only select external tasks that are currently suspended

cibseven/cibseven

engine-rest/engine-rest/src/main/java/org/cibseven/bpm/engine/rest/dto/externaltask/ExternalTaskQueryDto.java
@@ -23,15 +23,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
 import org.cibseven.bpm.engine.ProcessEngine;
 import org.cibseven.bpm.engine.externaltask.ExternalTaskQuery;
 import org.cibseven.bpm.engine.rest.dto.AbstractQueryDto;
 import org.cibseven.bpm.engine.rest.dto.CamundaQueryParam;
-import org.cibseven.bpm.engine.rest.dto.converter.BooleanConverter;
-import org.cibseven.bpm.engine.rest.dto.converter.DateConverter;
-import org.cibseven.bpm.engine.rest.dto.converter.LongConverter;
-import org.cibseven.bpm.engine.rest.dto.converter.StringListConverter;
-import org.cibseven.bpm.engine.rest.dto.converter.StringSetConverter;
+import org.cibseven.bpm.engine.rest.dto.VariableQueryParameterDto;
+import org.cibseven.bpm.engine.rest.dto.converter.*;
+import org.cibseven.bpm.engine.rest.exception.InvalidRequestException;
 
 /**
  * @author Thorben Lindhauer
@@ -62,7 +62,11 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   protected String executionId;
   protected String processInstanceId;
   protected List<String> processInstanceIdIn;
+  protected String processDefinitionKey;
+  protected String[] processDefinitionKeyIn;
   protected String processDefinitionId;
+  protected String processDefinitionName;
+  protected String processDefinitionNameLike;
   protected Boolean active;
   protected Boolean suspended;
   protected Boolean withRetriesLeft;
@@ -71,6 +75,10 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   protected List<String> tenantIds;
   protected Long priorityHigherThanOrEquals;
   protected Long priorityLowerThanOrEquals;
+  protected Boolean variableNamesIgnoreCase;
+  protected Boolean variableValuesIgnoreCase;
+
+  private List<VariableQueryParameterDto> processVariables;
 
   public ExternalTaskQueryDto() {
   }
@@ -143,9 +151,29 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     return processDefinitionId;
   }
 
+  @CamundaQueryParam("processDefinitionKey")
+  public void setProcessDefinitionKey(String processDefinitionKey) {
+    this.processDefinitionKey = processDefinitionKey;
+  }
+
+  @CamundaQueryParam(value = "processDefinitionKeyIn", converter = StringArrayConverter.class)
+  public void setProcessDefinitionKeyIn(String[] processDefinitionKeyIn) {
+    this.processDefinitionKeyIn = processDefinitionKeyIn;
+  }
+
   @CamundaQueryParam("processDefinitionId")
   public void setProcessDefinitionId(String processDefinitionId) {
     this.processDefinitionId = processDefinitionId;
+  }
+
+  @CamundaQueryParam("processDefinitionName")
+  public void setProcessDefinitionName(String processDefinitionName) {
+    this.processDefinitionName = processDefinitionName;
+  }
+
+  @CamundaQueryParam("processDefinitionNameLike")
+  public void setProcessDefinitionNameLike(String processDefinitionNameLike) {
+    this.processDefinitionNameLike = processDefinitionNameLike;
   }
 
   @CamundaQueryParam(value = "active", converter = BooleanConverter.class)
@@ -180,6 +208,21 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
   @CamundaQueryParam(value="priorityHigherThanOrEquals", converter = LongConverter.class)
   public void setPriorityHigherThanOrEquals(Long priorityHigherThanOrEquals) {
     this.priorityHigherThanOrEquals = priorityHigherThanOrEquals;
+  }
+
+  @CamundaQueryParam(value = "variableNamesIgnoreCase", converter = BooleanConverter.class)
+  public void setVariableNamesIgnoreCase(Boolean variableNamesCaseInsensitive) {
+    this.variableNamesIgnoreCase = variableNamesCaseInsensitive;
+  }
+
+  @CamundaQueryParam(value ="variableValuesIgnoreCase", converter = BooleanConverter.class)
+  public void setVariableValuesIgnoreCase(Boolean variableValuesCaseInsensitive) {
+    this.variableValuesIgnoreCase = variableValuesCaseInsensitive;
+  }
+
+  @CamundaQueryParam(value = "processVariables", converter = VariableListConverter.class)
+  public void setProcessVariables(List<VariableQueryParameterDto> processVariables) {
+    this.processVariables = processVariables;
   }
 
   @CamundaQueryParam(value="priorityLowerThanOrEquals", converter = LongConverter.class)
@@ -235,8 +278,20 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     if (processInstanceIdIn != null && !processInstanceIdIn.isEmpty()) {
       query.processInstanceIdIn(processInstanceIdIn.toArray(new String[0]));
     }
+    if (processDefinitionKey != null) {
+      query.processDefinitionKey(processDefinitionKey);
+    }
+    if (processDefinitionKeyIn != null && processDefinitionKeyIn.length > 0) {
+      query.processDefinitionKeyIn(processDefinitionKeyIn);
+    }
     if (processDefinitionId != null) {
       query.processDefinitionId(processDefinitionId);
+    }
+    if (processDefinitionName != null) {
+      query.processDefinitionName(processDefinitionName);
+    }
+    if (processDefinitionNameLike != null) {
+      query.processDefinitionNameLike(processDefinitionNameLike);
     }
     if (active != null && active) {
       query.active();
@@ -255,6 +310,38 @@ public class ExternalTaskQueryDto extends AbstractQueryDto<ExternalTaskQuery> {
     }
     if (noRetriesLeft != null && noRetriesLeft) {
       query.noRetriesLeft();
+    }
+    if(variableValuesIgnoreCase != null && variableValuesIgnoreCase) {
+      query.matchVariableValuesIgnoreCase();
+    }
+    if(variableNamesIgnoreCase != null && variableNamesIgnoreCase) {
+      query.matchVariableNamesIgnoreCase();
+    }
+    if (processVariables != null) {
+      for (VariableQueryParameterDto variableQueryParam : processVariables) {
+        String variableName = variableQueryParam.getName();
+        String op = variableQueryParam.getOperator();
+        Object variableValue = variableQueryParam.resolveValue(objectMapper);
+        if (op.equals(VariableQueryParameterDto.EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.NOT_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueNotEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.GREATER_THAN_OPERATOR_NAME)) {
+          query.processVariableValueGreaterThan(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.GREATER_THAN_OR_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueGreaterThanOrEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LESS_THAN_OPERATOR_NAME)) {
+          query.processVariableValueLessThan(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LESS_THAN_OR_EQUALS_OPERATOR_NAME)) {
+          query.processVariableValueLessThanOrEquals(variableName, variableValue);
+        } else if (op.equals(VariableQueryParameterDto.LIKE_OPERATOR_NAME)) {
+          query.processVariableValueLike(variableName, String.valueOf(variableValue));
+        } else if (op.equals(VariableQueryParameterDto.NOT_LIKE_OPERATOR_NAME)) {
+          query.processVariableValueNotLike(variableName, String.valueOf(variableValue));
+        } else {
+          throw new InvalidRequestException(Response.Status.BAD_REQUEST, "Invalid process variable comparator specified: " + op);
+        }
+      }
     }
     if (workerId != null) {
       query.workerId(workerId);

engine/src/main/java/org/cibseven/bpm/engine/externaltask/ExternalTaskQuery.java
@@ -16,10 +16,11 @@
  */
 package org.cibseven.bpm.engine.externaltask;
 
+import org.cibseven.bpm.engine.query.Query;
+
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
-
-import org.cibseven.bpm.engine.query.Query;
 
 /**
  * @author Thorben Lindhauer
@@ -85,9 +86,34 @@ public interface ExternalTaskQuery extends Query<ExternalTaskQuery, ExternalTask
   ExternalTaskQuery processInstanceIdIn(String... processInstanceIdIn);
 
   /**
+   * Only select tasks which are part of a process instance which has the given
+   * process definition key.
+   */
+  ExternalTaskQuery processDefinitionKey(String processDefinitionKey);
+
+  /**
+   * Only select tasks which are part of a process instance which has one of the
+   * given process definition keys.
+   */
+  ExternalTaskQuery processDefinitionKeyIn(String... processDefinitionKeys);
+
+  /**
    * Only select external tasks that belong to an instance of the given process definition
    */
   ExternalTaskQuery processDefinitionId(String processDefinitionId);
+
+  /**
+   * Only select tasks which are part of a process instance which has the given
+   * process definition name.
+   */
+  ExternalTaskQuery processDefinitionName(String processDefinitionName);
+
+  /**
+   * Only select tasks which are part of a process instance which process definition
+   * name  is like the given parameter.
+   * The syntax is that of SQL: for example usage: nameLike(%processDefinitionName%)
+   */
+  ExternalTaskQuery processDefinitionNameLike(String processDefinitionName);
 
   /**
    * Only select external tasks that belong to an instance of the given activity
@@ -116,6 +142,68 @@ public interface ExternalTaskQuery extends Query<ExternalTaskQuery, ExternalTask
    * @return the builded external task query
    */
   ExternalTaskQuery priorityLowerThanOrEquals(long priority);
+
+  /**
+   * Only select tasks which have are part of a process that have a variable
+   * with the given name set to the given value.
+   */
+  ExternalTaskQuery processVariableValueEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which have a variable with the given name, but
+   * with a different value than the passed value.
+   * Byte-arrays and {@link Serializable} objects (which are not primitive type wrappers)
+   * are not supported.
+   */
+  ExternalTaskQuery processVariableValueNotEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and matching the given value.
+   * The syntax is that of SQL: for example usage: valueLike(%value%)
+   */
+  ExternalTaskQuery processVariableValueLike(String variableName, String variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and not matching the given value.
+   * The syntax is that of SQL: for example usage: valueNotLike(%value%)
+   */
+  ExternalTaskQuery processVariableValueNotLike(String variableName, String variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than the given one.
+   */
+  ExternalTaskQuery processVariableValueGreaterThan(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than or equal to the given one.
+   */
+  ExternalTaskQuery processVariableValueGreaterThanOrEquals(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value less than the given one.
+   */
+  ExternalTaskQuery processVariableValueLessThan(String variableName, Object variableValue);
+
+  /**
+   * Only select tasks which are part of a process that have a variable
+   * with the given name and a value greater than or equal to the given one.
+   */
+  ExternalTaskQuery processVariableValueLessThanOrEquals(String variableName, Object variableValue);
+
+  /**
+   * All queries for task-, process- and case-variables will match the variable names in a case-insensitive way.
+   */
+  ExternalTaskQuery matchVariableNamesIgnoreCase();
+
+  /**
+   * All queries for task-, process- and case-variables will match the variable values in a case-insensitive way.
+   */
+  ExternalTaskQuery matchVariableValuesIgnoreCase();
 
   /**
    * Only select external tasks that are currently suspended