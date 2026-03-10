25c0685092d479988804f795b6c17a9928bbf94d
Oliver Specht <oliver.specht@cib.de>
2026-03-10 10:13:20 +0100

    junit 5 with copilot imports

--- engine-cdi/core/src/test/java/org/cibseven/bpm/engine/cdi/test/impl/el/ElTest.java
+++ engine-cdi/core/src/test/java/org/cibseven/bpm/engine/cdi/test/impl/el/ElTest.java
@@ -16,7 +16,7 @@
  */
 package org.cibseven.bpm.engine.cdi.test.impl.el;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 
