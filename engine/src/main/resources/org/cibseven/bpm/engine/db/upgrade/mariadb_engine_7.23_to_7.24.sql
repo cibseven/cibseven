4c84f360d92db6c0dcca870cf4e8f26a0c47028c
vladimirg <vladimir.gribko@cib.de>
2026-03-13 09:29:57 +0100

    fix(db): remove not needed lash from mariadb update script

--- engine/src/main/resources/org/cibseven/bpm/engine/db/upgrade/mariadb_engine_7.23_to_7.24.sql
+++ engine/src/main/resources/org/cibseven/bpm/engine/db/upgrade/mariadb_engine_7.23_to_7.24.sql
@@ -13,7 +13,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
---/
+--
 
 insert into ACT_GE_SCHEMA_LOG
 values ('1300', CURRENT_TIMESTAMP, '7.24.0');
